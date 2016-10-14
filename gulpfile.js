5d7803aa64d02b7cae2ba6c501a2f540a94f4ef5
Philippe Damen <mr.deleeuw@gmail.com>
2016-10-14 09:15:40 +0200

    Added gulp-chug

--- gulpfile.js
+++ gulpfile.js
@@ -5,4 +5,25 @@
  * For building the backend assets, you should run gulp inside
  * the ./resources/assets/back/ folder
  */
-require('./resources/assets/front/gulpfile.js');
+
+var gulp = require('gulp');
+var chug = require( 'gulp-chug' );
+
+function defaultTask()
+{
+	gulp.src( './resources/assets/front/gulpfile.js', { read: false } )
+		.pipe( chug({
+			tasks: ['watch']
+		}) )
+}
+
+gulp.task('watch:front', defaultTask);
+gulp.task('default', defaultTask);
+gulp.task('watch', defaultTask);
+
+gulp.task( 'watch:back', function () {
+	gulp.src( './resources/assets/back/gulpfile.js', { read: false } )
+		.pipe( chug({
+			tasks: ['watch']
+		}) )
+} );
