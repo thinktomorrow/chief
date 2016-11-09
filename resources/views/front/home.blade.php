aedd8ac02e4f0abbbd6e5a2b50896c29bb84d7c3
Ben Cavens <cavensben@gmail.com>
2016-11-09 12:19:59 +0100

    detect if javascript is enabled

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
+<!--[if lt IE 9]><html class="lt-ie9 no-js"> <![endif]-->
+<!--[if gte IE 9]><!--> <html class="no-js" lang="{{ app()->getLocale() }}"><!--<![endif]-->
 <html>
     <head>
+        <script>
+            // If javascript is active, we can assign the 'js' class to our html root instead of 'no-js' class
+            // Only use this class if Modernizr is not being used
+            (function(html){html.className = html.className.replace(/\bno-js\b/,'js')})(document.documentElement);
+        </script>
         <title>Laravel</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
