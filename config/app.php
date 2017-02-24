044b1935af9a0b5fa545d88dd93f1c4251c42829
Ben Cavens <cavensben@gmail.com>
2017-02-24 17:10:37 +0100

    add preliminary routes

--- config/app.php
+++ config/app.php
@@ -236,6 +236,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Locale' => \Thinktomorrow\Locale\Facades\LocaleFacade::class,
+
     ],
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,43 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/**
+ * -----------------------------------------------------------------
+ * ADMIN ROUTES
+ * -----------------------------------------------------------------
+ */
+Route::group(['prefix' => 'admin','middleware' =>'auth' ,'namespace' => 'Back'],function(){
 
+    // FOR DEVS ONLY!
+    Route::get('translations/{slug}/lines/create',['middleware' => 'auth.superadmin', 'as' => 'back.trans.lines.create','uses' => '\Chief\Trans\Controllers\TranslationLineController@create']);
+    Route::post('translations/{slug}/lines',['middleware' => 'auth.superadmin', 'as' => 'back.trans.lines.store','uses' => '\Chief\Trans\Controllers\TranslationLineController@store']);
+    Route::get('translations/{slug}',['as' => 'back.trans.edit','uses' => '\Chief\Trans\Controllers\TranslationController@edit']);
+    Route::put('translations/{group_id}',['as' => 'back.trans.update','uses' => '\Chief\Trans\Controllers\TranslationController@update']);
+
+    Route::get('/',['as' => 'back.dashboard','uses' => DashboardController::class.'@show']);
+
+});
+
+/**
+ * -----------------------------------------------------------------
+ * PUBLIC LANGUAGE SWITCHER
+ * -----------------------------------------------------------------
+ */
+Route::get('lang',['as' => 'lang.switch','uses' => LanguageSwitcher::class.'@store']);
+
+/**
+ * -----------------------------------------------------------------
+ * PUBLIC TRANSLATABLE ROUTES
+ * -----------------------------------------------------------------
+ */
+Route::group(['prefix' => Locale::set(),'namespace' => 'Front'],function(){
+
+    Route::get('/', ['as' => 'pages.home', 'uses' => function(){
+        return view('front.home');
+    }]);
+
+});
+
+// SETUP ROUTES
 Route::get('/', function () {
-    return view('welcome');
+    return view('setup.welcome');
 });
