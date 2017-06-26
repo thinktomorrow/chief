29a3dc09a79380cc7c92471e4078827f6aa63921
Ben Cavens <cavensben@gmail.com>
2017-06-26 16:38:35 +0200

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,12 @@ Route::group(['prefix' => 'admin','middleware' =>'auth' ,'namespace' => 'Back'],
     Route::get('articles','ArticlesController@index')->name('admin.articles.index');
 
     // FOR DEVS ONLY!
-    Route::get('translations/{slug}/lines/create',['middleware' => 'auth.superadmin', 'as' => 'back.trans.lines.create','uses' => '\Chief\Trans\Controllers\TranslationLineController@create']);
-    Route::post('translations/{slug}/lines',['middleware' => 'auth.superadmin', 'as' => 'back.trans.lines.store','uses' => '\Chief\Trans\Controllers\TranslationLineController@store']);
-    Route::get('translations/{slug}',['as' => 'back.trans.edit','uses' => '\Chief\Trans\Controllers\TranslationController@edit']);
-    Route::put('translations/{group_id}',['as' => 'back.trans.update','uses' => '\Chief\Trans\Controllers\TranslationController@update']);
+//    Route::get('translations/{slug}/lines/create',['middleware' => 'auth.superadmin', 'as' => 'back.trans.lines.create','uses' => '\Chief\Trans\Controllers\TranslationLineController@create']);
+//    Route::post('translations/{slug}/lines',['middleware' => 'auth.superadmin', 'as' => 'back.trans.lines.store','uses' => '\Chief\Trans\Controllers\TranslationLineController@store']);
+//    Route::get('translations/{slug}',['as' => 'back.trans.edit','uses' => '\Chief\Trans\Controllers\TranslationController@edit']);
+//    Route::put('translations/{group_id}',['as' => 'back.trans.update','uses' => '\Chief\Trans\Controllers\TranslationController@update']);
 
     Route::get('/',['as' => 'admin.home','uses' => HomeController::class.'@show']);
-
 });
 
 /**
@@ -25,7 +24,7 @@ Route::group(['prefix' => 'admin','middleware' =>'auth' ,'namespace' => 'Back'],
  * PUBLIC LANGUAGE SWITCHER
  * -----------------------------------------------------------------
  */
-Route::get('lang',['as' => 'lang.switch','uses' => LanguageSwitcher::class.'@store']);
+Route::get('lang',['as' => 'lang.switch','uses' => Front\LanguageSwitcher::class.'@store']);
 
 /**
  * -----------------------------------------------------------------
