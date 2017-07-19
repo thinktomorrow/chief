5d236ef509dfd2904b3a498758c4004ea564f46b
Kingjohnny <kingjohnny@iMac-van-Kingjohnny.local>
2017-07-19 09:16:19 +0200

    Dashboard

--- resources/views/back/home.blade.php
+++ resources/views/back/home.blade.php
@@ -1,13 +1,32 @@
 @extends('back._layouts.master')
 
+@section('page-title')
+Dashboard
+@stop
+
+@section('topbar-right')
+
+@stop
+
 @section('content')
+<div class="row">
+  <div class="col-md-4">
+  <div class="panel panel-tile text-center br-a br-grey">
+    <div class="panel-body">
+      @role('Admin')
+      <img src="{{ asset('assets/back/img/dashboard-chief.png')}}" alt="Dashboard">
+      <h1 class="fs30 mt5 mbn">Welkom {{ Auth::user()->firstname }}</h1>
+      <h6 class="text-system">Your new name will be "{{ Auth::user()->getAlterEgo() }}"</h6>
 
-    @role('Admin')
+      @endrole
+      @role('User')
       <img src="{{ asset('assets/back/img/dashboard.png')}}" alt="Dashboard">
-        <h1>Welkom op jouw Admin dashboard</h1>
-    @endrole
+      <h1 class="fs30 mt5 mbn">Welkom {{ Auth::user()->firstname }}</h1>
+      <h6 class="text-system">Your new name will be "{{ Auth::user()->getAlterEgo() }}"</h6>
 
-    @role('User')
-        Welkom op jouw User dashboard
-    @endrole
+      @endrole
+    </div>
+  </div>
+</div>
+</div>
 @stop
---
+++ resources/views/back/media/gallery.blade.php
@@ -0,0 +1,45 @@
+<section class="media-gallery mh15 pv15">
+  <div id="mix-container">
+    <div class="row">
+      @if($library->isEmpty())
+      <div class="text-center">
+        <img src="{{ asset('assets/back/img/placeholder.png')}}" alt="holder">
+      </div>
+      @endif
+      @foreach($library as $media)
+        <!-- image thumb -->
+        <div class="mix col-sm-6 col-md-6 col-lg-3">
+          <div class="panel media pbn">
+            <div class="panel-heading">
+              <span class="panel-icon pull-left">
+                <i class="fa fa-image"></i>
+              </span>
+              <span class="panel-title overflow-ellipsis">
+                {{ $media->getFilename() }}
+              </span>
+              <span class="panel-controls">
+                <div class="checkbox-delete">
+                  <label for="checkboxMedia-{{ $media->id }}">
+                    <h3 class="text-default pt5 pn mn"><i class="fa fa-check-circle"></i></h3>
+                  </label>
+                  <input class="hidden" type="checkbox" id="checkboxMedia-{{ $media->id }}" name="imagestoremove[]" value="{{ $media->id }}">
+                </div>
+              </span>
+            </div>
+            <div class="panel-body pn" id="showDetailPanel">
+            <figure class="mn">
+              <picture>
+                <img src="{{ $media->getPathForSize('large') }}" class="img-responsive" title="{{ $media->getFilename() }}">
+              </picture>
+            </figure>
+          </div>
+          </div>
+        </div>
+      @endforeach
+    </div>
+    <div class="row text-center">
+      {{ $library->render() }}
+    </div>
+  </div>
+</section>
+
