3c0d88563f1ad8e877f3734870ec3bf158c45cc6
Kingjohnny <kingjohnny@iMac-van-Kingjohnny.local>
2017-07-24 09:18:00 +0200

    Fix in migration

--- database/migrations/2017_07_23_125443_add_collection_to_asset.php
+++ database/migrations/2017_07_23_125443_add_collection_to_asset.php
@@ -14,7 +14,7 @@ class AddCollectionToAsset extends Migration
     public function up()
     {
         Schema::table('assets', function (Blueprint $table){
-            $table->text('type')->default('default');
+            $table->text('type')->nullable();
         });
     }
 
