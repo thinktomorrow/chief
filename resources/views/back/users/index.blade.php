3661737ee8f79b7859e3ccae2131e576540c2bb8
Bob Dries <bob3s@hotmail.com>
2017-07-25 12:44:04 +0200

    added extra javascript for showing hiding panels

--- resources/views/back/users/index.blade.php
+++ resources/views/back/users/index.blade.php
@@ -62,6 +62,7 @@
 					$("#btnCancelUser").removeClass("hidden");
 					$("body").addClass( "sb-r-o" );
 					$("body").removeClass( "sb-r-c" );
+					$("body").addClass("sb-l-m");
 					$("#OverlayUser").show();
 					$( "#focusField" ).focus();
 				});
@@ -80,8 +81,9 @@
 					$("#OverlayUser").hide();
 					$("body").removeClass( "sb-r-o" );
 					$("body").addClass( "sb-r-c" );
+					$("body").removeClass("sb-l-m");
 					$(".detail-open").removeClass("detail-open");
-					$(document.body).removeClass("sidebar-media-open");
+					$("body").removeClass("sidebar-media-open");
 				});
 
 				$("#OverlayUser").click(function(){
@@ -90,8 +92,9 @@
 					$("#OverlayUser").hide();
 					$("body").removeClass( "sb-r-o" );
 					$("body").addClass( "sb-r-c" );
+					$("body").removeClass("sb-l-m");
 					$(".detail-open").removeClass("detail-open");
-					$(document.body).removeClass("sidebar-media-open");
+					$("body").removeClass("sidebar-media-open");
 
 				});
       });
@@ -99,7 +102,8 @@
 			$(".showEditUser").click(function(){
 				$('.editUser-' + this.dataset.sidebarId).addClass('detail-open');
 				// $('.overlay').show(); // Show overlay when detail is active
-				$(document.body).addClass("sidebar-media-open");
+				$("body").addClass("sidebar-media-open");
+				$("body").addClass("sb-l-m");
 				$("#btnNewUser").addClass("hidden");
 				$("#btnCancelUser").removeClass("hidden");
 				$("#OverlayUser").show();
