231e6c24a99c9eac7cd5bfd633e66b3de1d520f7
Bob Dries <bob3s@hotmail.com>
2017-07-25 12:42:21 +0200

    created new role

--- resources/views/back/roles/_partials/newrole.blade.php
+++ resources/views/back/roles/_partials/newrole.blade.php
@@ -3,51 +3,59 @@
 
   <!-- Start: Sidebar Right Content -->
   <div class="sidebar-right-content nano-content p10">
-    <h4 class="tray-title">Nieuwe rol</h5>
+    <h4 class="tray-title">Nieuwe rol</h4>
       <div class="panel panel-tile card-create">
-        <div class="panel-body text-center ">
-          <img src="{{ asset('assets/img/logo.png') }}" alt="test" class="mw100">
-        </div>
         <div class="panel-body">
 
-          <!-- STARTING THE FORM -->
-          <form action="{{ route('users.store') }}" method="POST">
-          {!! csrf_field() !!}
-            <!-- NAAM INPUT -->
-            <div class="input-group mv15">
-              <span class="input-group-addon text-muted">
-                <i class="fa fa-user"></i>
-              </span>
-              <input id="focusField" type="text" name="firstname" value="{{ old('firstname') }}" class="form-control" placeholder="Voornaam">
-              <input type="text" name="lastname" value="{{ old('lastname') }}" class="form-control" placeholder="Naam">
-            </div>
-            <!-- EMAIL INPUT -->
-            <div class="input-group mv15">
-              <span class="input-group-addon text-muted">
-                <i class="fa fa-envelope-o"></i>
-              </span>
-              <input type="email" name="email" class="form-control" value="{{ old('email') }}" placeholder="E-mail">
-            </div>
+          <form action="{{ route('roles.store') }}" method="POST">
+      			{!! csrf_field() !!}
 
-            <!-- ROLE INPUT -->
-            <div class="section mv15 admin-form">
-              <div class="option-group">
-                @foreach (\App\Role::all() as $role)
-                  <p><label class="option option-primary">
-                  <input type="checkbox" name="{{ $role->name }}" value="{{ $role->id }}">
-                  <span class="checkbox"></span>{{ $role->name }}</label></p>
-                @endforeach
-              </div>
-            </div>
+          <div class="input-group mv15">
+            <span class="input-group-addon text-muted">
+              <i class="fa fa-key"></i>
+            </span>
+            <input id="focusField" type="text" name="name" value="" class="form-control" placeholder="Rolnaam">
           </div>
 
-          <div class="panel-footer">
-            <button type="submit" value="Submit" class="btn btn-success btn-block"><i class="fa fa-plus mr10" aria-hidden="true"></i>TOEVOEGEN</button>
-          </div>
-        </form>
+      		<h5 class="title-divider text-muted mt30 mb10">Permissies</h5>
+           <?php
+             $i = 1;
+           ?>
+
+          @foreach(\App\Permission::getPermissionsForIndex() as $model => $permissions)
+            <div id="tree{{ $i }}" class="treeview-item">
+      				<ul id="treeData">
+      					<li><input type="checkbox" id="chkSelectAll{{ $i }}" name="chkSelectAll{{ $i }}" onclick="checkAll('tree{{ $i }}')" class="mr5">{{ $model }}
+      						<ul>
+                    @foreach($permissions as $id => $permission)
+          						{{-- <input type="checkbox" value="{{ $permission }}" name="permissions[]" {{ $role->permissions->contains($permission->id) ? 'checked' : '' }}> --}}
+          						<li id="{{ $permission }}"><input type="checkbox" value="{{ $id }}" class="mr5">{{ ucfirst($permission) }}</li><br>
+          					@endforeach
+      						</ul>
+      					</li>
+      				</ul>
+              <?php $i++; ?>
+            </div>
+          @endforeach
+
+            <button type="submit" value="Submit" class="btn btn-success btn-block mt35"><i class="fa fa-plus mr10" aria-hidden="true"></i>ROL OPSLAAN</button>
+      		</form>
         <!-- END OF THE FORM -->
 
       </div>
   </div>
 </aside>
 <!-- End: Right Sidebar -->
+@push('custom-scripts')
+    <script>
+      function checkAll(divid) {
+        if ($('#' + divid + ' input:checkbox:checked').length > 1)
+        {
+          $('#' + divid + ' :checkbox:enabled').prop('checked', false);
+        }else{
+          $('#' + divid + ' :checkbox:enabled').prop('checked', true);
+        }
+
+      }
+    </script>
+	@endpush
