00fe9e5045323873e4e5b815d0cec29f136559e6
Philippe Damen <mr.deleeuw@gmail.com>
2018-04-13 15:20:22 +0200

    modal/ notification pages spirit

--- resources/views/spirit/components/sidebar.php
+++ resources/views/spirit/components/sidebar.php
@@ -1,10 +1,36 @@
 <?php include(__DIR__.'/../_partials/header.php'); ?>
 
-<section id="sidebar">
-    <h2>Sidebar</h2>
-    <h3 id="tabs1">Basic sidebar</h3>
+<section class="stack-l components" id="sidebars">
+    <h1>Sidebar</h1>
+    <hr>
 
-    ...
+    <modal type="sidebar" id="exampleModal2" title="Welcome to this sidebar">
+        <div v-cloak>
+            <div class="stack-s">
+                <a class="btn">First link</a><br>
+                <hr>
+                <a class="btn">Secon link</a><br>
+                <hr>
+                <a class="btn">Third link</a>
+            </div>
+
+            <div class="stack-m">
+                <a class="btn btn-secondary" @click="closeModal('exampleModal2')">Oke, Got it</a>
+            </div>
+        </div>
+    </modal>
+
+    <div class="stack-m">
+        <button class="btn btn-secondary" @click="showModal('exampleModal2')">Show sidebar</button>
+    </div>
+
+    <p>The modal component can also be used as an offcanvas sidebar. This is ideal for presenting secondary information, context screens or small form fields. Just add an attribute <em>type="sidebar"</em> on the modal element.</p>
+    <pre>
+        <code class="html">
+<?= htmlentities('<modal type="sidebar" id="exampleModal"></modal>' ); ?>
+        </code>
+    </pre>
+
 </section>
 
 <?php include(__DIR__.'/../_partials/footer.php'); ?>
