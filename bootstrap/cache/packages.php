0c5c237fec35e13593b69af13c1a6d2f7aeebb44
Philippe Damen <mr.deleeuw@gmail.com>
2018-06-05 09:50:43 +0000

    Apply fixes from StyleCI

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -1,117 +1,117 @@
-<?php return array (
-  'dimsav/laravel-translatable' => 
-  array (
-    'providers' => 
-    array (
+<?php return array(
+  'dimsav/laravel-translatable' =>
+  array(
+    'providers' =>
+    array(
       0 => 'Dimsav\\Translatable\\TranslatableServiceProvider',
     ),
   ),
-  'intervention/image' => 
-  array (
-    'providers' => 
-    array (
+  'intervention/image' =>
+  array(
+    'providers' =>
+    array(
       0 => 'Intervention\\Image\\ImageServiceProvider',
     ),
-    'aliases' => 
-    array (
+    'aliases' =>
+    array(
       'Image' => 'Intervention\\Image\\Facades\\Image',
     ),
   ),
-  'consoletvs/support' => 
-  array (
-    'providers' => 
-    array (
+  'consoletvs/support' =>
+  array(
+    'providers' =>
+    array(
       0 => 'ConsoleTVs\\Support\\SupportServiceProvider',
     ),
   ),
-  'consoletvs/charts' => 
-  array (
-    'providers' => 
-    array (
+  'consoletvs/charts' =>
+  array(
+    'providers' =>
+    array(
       0 => 'ConsoleTVs\\Charts\\ChartsServiceProvider',
     ),
-    'aliases' => 
-    array (
+    'aliases' =>
+    array(
       'Charts' => 'ConsoleTVs\\Charts\\Facades\\Charts',
     ),
   ),
-  'spatie/laravel-image-optimizer' => 
-  array (
-    'providers' => 
-    array (
+  'spatie/laravel-image-optimizer' =>
+  array(
+    'providers' =>
+    array(
       0 => 'Spatie\\LaravelImageOptimizer\\ImageOptimizerServiceProvider',
     ),
-    'aliases' => 
-    array (
+    'aliases' =>
+    array(
       'ImageOptimizer' => 'Spatie\\LaravelImageOptimizer\\Facades\\ImageOptimizer',
     ),
   ),
-  'thinktomorrow/locale' => 
-  array (
-    'providers' => 
-    array (
+  'thinktomorrow/locale' =>
+  array(
+    'providers' =>
+    array(
       0 => 'Thinktomorrow\\Locale\\LocaleServiceProvider',
     ),
   ),
-  'laravel/tinker' => 
-  array (
-    'providers' => 
-    array (
+  'laravel/tinker' =>
+  array(
+    'providers' =>
+    array(
       0 => 'Laravel\\Tinker\\TinkerServiceProvider',
     ),
   ),
-  'laravelcollective/html' => 
-  array (
-    'providers' => 
-    array (
+  'laravelcollective/html' =>
+  array(
+    'providers' =>
+    array(
       0 => 'Collective\\Html\\HtmlServiceProvider',
     ),
-    'aliases' => 
-    array (
+    'aliases' =>
+    array(
       'Form' => 'Collective\\Html\\FormFacade',
       'Html' => 'Collective\\Html\\HtmlFacade',
     ),
   ),
-  'spatie/laravel-permission' => 
-  array (
-    'providers' => 
-    array (
+  'spatie/laravel-permission' =>
+  array(
+    'providers' =>
+    array(
       0 => 'Spatie\\Permission\\PermissionServiceProvider',
     ),
   ),
-  'spatie/laravel-medialibrary' => 
-  array (
-    'providers' => 
-    array (
+  'spatie/laravel-medialibrary' =>
+  array(
+    'providers' =>
+    array(
       0 => 'Spatie\\MediaLibrary\\MediaLibraryServiceProvider',
     ),
   ),
-  'thinktomorrow/assetlibrary' => 
-  array (
-    'providers' => 
-    array (
+  'thinktomorrow/assetlibrary' =>
+  array(
+    'providers' =>
+    array(
       0 => 'Thinktomorrow\\AssetLibrary\\AssetLibraryServiceProvider',
     ),
   ),
-  'barryvdh/laravel-debugbar' => 
-  array (
-    'providers' => 
-    array (
+  'barryvdh/laravel-debugbar' =>
+  array(
+    'providers' =>
+    array(
       0 => 'Barryvdh\\Debugbar\\ServiceProvider',
     ),
-    'aliases' => 
-    array (
+    'aliases' =>
+    array(
       'Debugbar' => 'Barryvdh\\Debugbar\\Facade',
     ),
   ),
-  'jenssegers/date' => 
-  array (
-    'providers' => 
-    array (
+  'jenssegers/date' =>
+  array(
+    'providers' =>
+    array(
       0 => 'Jenssegers\\Date\\DateServiceProvider',
     ),
-    'aliases' => 
-    array (
+    'aliases' =>
+    array(
       'Date' => 'Jenssegers\\Date\\Date',
     ),
   ),
