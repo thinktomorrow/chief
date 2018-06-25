ad6a57b6cd6be02caace5bf588892787df4b7e44
Ben Cavens <cavensben@gmail.com>
2018-06-25 15:12:39 +0200

    better display of nested item

--- resources/views/back/menu/_partials/_rowitem.blade.php
+++ resources/views/back/menu/_partials/_rowitem.blade.php
@@ -1,6 +1,9 @@
 <div class="row">
     <div class="column center-y {{ isset($level) ? 'indent-'.$level : '' }}">
         {{--<i class="icon icon-menu inline text-border tree-parent"></i>--}}
+        @if(isset($level) && $level > 0)
+            <span class="icon icon-arrow-right text-border inline-s tree-parent"></span>
+        @endif
         <a href="{{ route('chief.back.menu.edit', $menuitem->id) }}">{{ $menuitem->label }}</a>
     </div>
 
