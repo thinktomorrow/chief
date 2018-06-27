89e755541d080b65331e3b65fab60e0952243435
Philippe Damen <philippe@thinktomorrow.be>
2018-06-27 13:20:46 +0200

    fix

--- src/Pages/Page.php
+++ src/Pages/Page.php
@@ -135,8 +135,7 @@ class Page extends Model implements TranslatableContract, HasMedia, ActsAsParent
      */
     public static function flattenForSelect()
     {
-        //->published()
-        return self::ignoreCollection()->get()->map(function (Page $page) {
+        return self::ignoreCollection()->published()->get()->map(function (Page $page) {
             return [
                 'id'    => $page->getRelationId(),
                 'label' => $page->getRelationLabel(),
