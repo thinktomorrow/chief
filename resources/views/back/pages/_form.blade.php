15f0ccadb8fbce2aa8275f6d31b79d379ca12f20
Philippe Damen <philippe@thinktomorrow.be>
2018-07-06 15:20:08 +0200

    fix translation tabs for one language

--- resources/views/back/pages/_form.blade.php
+++ resources/views/back/pages/_form.blade.php
@@ -5,26 +5,19 @@
                 <h2 class="formgroup-label">Titel</h2>
             </div>
             <div class="formgroup-input column-8">
-                <tabs>
+                @if(count($page->availableLocales()) > 1) 
+                    <tabs v-cloak>
+                        @foreach($page->availableLocales() as $locale)
+                            <tab name="{{ $locale }}" :options="{ hasErrors: errors.has('trans.{{ $locale }}.title')}">
+                                @include('chief::back._pages._partials.title-form')
+                            </tab>
+                        @endforeach
+                    </tabs>
+                @else
                     @foreach($page->availableLocales() as $locale)
-
-                        <tab name="{{ $locale }}" :options="{ hasErrors: errors.has('trans.{{ $locale }}.title')}">
-                            <div class="stack-s">
-                                <label for="trans-{{ $locale }}-title">Titel</label>
-                                <input type="text" name="trans[{{ $locale }}][title]" id="trans-{{ $locale }}-title" class="input inset-s" placeholder="Titel" value="{{ old('trans.'.$locale.'.title', $page->translateForForm($locale,'title')) }}">
-                                <span class="stack inline-block text-default">
-                                    <b>Permalink:</b> https://crius-group.com/
-                                    <input class="inset-s" type="text" name="trans[{{$locale}}][slug]" value="{{ old('trans.'.$locale.'.slug', $page->translateForForm($locale,'slug')) }}">
-                                </span>
-                            </div>
-
-                            <error class="caption text-warning" field="trans.{{ $locale }}.title" :errors="errors.get('trans.{{ $locale }}')"></error>
-                            <error class="caption text-warning" field="trans.{{ $locale }}.slug" :errors="errors.get('trans.{{ $locale }}')"></error>
-
-                        </tab>
-
+                        @include('chief::back.pages._partials.title-form')
                     @endforeach
-                </tabs>
+                @endif
             </div>
         </section>
 
@@ -57,26 +50,19 @@
                 <p class="caption">Titel en omschrijving van het pagina zoals het in search engines (o.a. google) wordt weergegeven.</p>
             </div>
             <div class="formgroup-input column-7">
-                <tabs>
+                @if(count($page->availableLocales()) > 1) 
+                    <tabs v-cloak>
+                        @foreach($page->availableLocales() as $locale)
+                            <tab name="{{ $locale }}" :options="{ hasErrors: errors.has('trans.{{ $locale }}.seo_title')}">
+                                @include('chief::back.pages._partials.seo-form')
+                            </tab>
+                        @endforeach
+                    </tabs>
+                @else
                     @foreach($page->availableLocales() as $locale)
-
-                        <tab name="{{ $locale }}" :options="{ hasErrors: errors.has('trans.{{ $locale }}.seo_title')}">
-                            <div class="stack-s">
-                                <label for="trans-{{ $locale }}-seo_title">Seo titel</label>
-                                <input type="text" name="trans[{{ $locale }}][seo_title]" id="trans-{{ $locale }}-seo_title" class="input inset-s" placeholder="Seo titel" value="{{ old('trans.'.$locale.'.seo_title',$page->translateForForm($locale,'seo_title')) }}">
-                            </div>
-
-                            <error class="caption text-warning" field="trans.{{ $locale }}.seo_title" :errors="errors.get('trans.{{ $locale }}')"></error>
-
-                            <div class="stack">
-                                <label for="trans-{{ $locale }}-seo_description">Seo omschrijving</label>
-                                <textarea class="inset-s" name="trans[{{ $locale }}][seo_description]" id="trans-{{ $locale }}-seo_description" cols="30" rows="10">{{ old('trans.'.$locale.'.seo_description',$page->translateForForm($locale,'seo_description')) }}</textarea>
-                            </div>
-                        </tab>
-
+                        @include('chief::back.pages._partials.seo-form')
                     @endforeach
-                </tabs>
-
+                @endif
                 <label for="seo-title"><i>Preview</i></label>
                 <div class="panel seo-preview --border inset bc-success">
                     <h2 class="text-information">SEO Titel</h2>
---
+++ resources/views/back/pages/_partials/title-form.blade.php
@@ -0,0 +1,11 @@
+<div class="stack-s">
+        <label for="trans-{{ $locale }}-title">Titel</label>
+        <input type="text" name="trans[{{ $locale }}][title]" id="trans-{{ $locale }}-title" class="input inset-s" placeholder="Titel" value="{{ old('trans.'.$locale.'.title', $page->translateForForm($locale,'title')) }}">
+        <span class="stack inline-block text-default">
+            <b>Permalink:</b> https://crius-group.com/
+            <input class="inset-s" type="text" name="trans[{{$locale}}][slug]" value="{{ old('trans.'.$locale.'.slug', $page->translateForForm($locale,'slug')) }}">
+        </span>
+    </div>
+
+    <error class="caption text-warning" field="trans.{{ $locale }}.title" :errors="errors.get('trans.{{ $locale }}')"></error>
+    <error class="caption text-warning" field="trans.{{ $locale }}.slug" :errors="errors.get('trans.{{ $locale }}')"></error>
