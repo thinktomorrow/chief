83252e96ddf6a84318b27f667fd3b9d2273b1258
philippe damen <mr.deleeuw@gmail.com>
2018-07-11 14:01:07 +0200

    Sortable reordering

--- src/Menu/Application/UpdateMenu.php
+++ src/Menu/Application/UpdateMenu.php
@@ -24,6 +24,8 @@ class UpdateMenu
             $menu->parent_id    = ($request->get('allow_parent') && $request->get('parent_id')) ? $request->get('parent_id') : null;
             $menu->page_id      = ($page_id = $request->get('page_id')) ? $this->getPage($request->get('page_id'))->id : null;
             $menu->order        = $request->get('order', 0);
+
+            $this->reorderAgainstSiblings($menu);
             $menu->save();
 
             $this->saveTranslations($request->get('trans'), $menu, [
@@ -33,11 +35,46 @@ class UpdateMenu
             DB::commit();
 
             return $menu->fresh();
+
         } catch (\Throwable $e) {
             DB::rollBack();
             throw $e;
         }
     }
+
+    private function reorderAgainstSiblings(MenuItem $menu)
+    {
+        $siblings = MenuItem::where('parent_id', $menu->parent_id)->whereNotIn('id', [$menu->id])->get();
+
+        $sequence = $siblings->pluck('order', 'id')->toArray();
+        asort($sequence);
+
+        if(in_array($menu->order, $sequence)){
+            foreach($sequence as $id => $order){
+                if($order < $menu->order){
+
+                }else{
+                    $sequence[$id]++;
+                }
+            }
+        }
+
+        $sequence = $sequence + [$menu->id => $menu->order];
+
+        asort($sequence);
+
+        $this->reorder($sequence);
+    }
+
+    private function reorder(array $sequence)
+    {
+        array_walk($sequence, function ($order, $id) {
+            MenuItem::withoutGlobalScope(SoftDeletingScope::class)
+                ->where('id', $id)
+                ->update(['order' => $order]);
+        });
+    }
+
 
     private function getPage($collection_id)
     {
