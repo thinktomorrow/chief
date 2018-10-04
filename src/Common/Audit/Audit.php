90357eebe3b0a5dac05dd001ecefa70ea5a712c7
Philippe Damen <philippe@thinktomorrow.be>
2018-10-04 16:45:24 +0200

    Fix audit page for getting model singular with fallback

--- src/Common/Audit/Audit.php
+++ src/Common/Audit/Audit.php
@@ -10,6 +10,8 @@ use Thinktomorrow\Chief\Users\User;
 
 class Audit extends Activity
 {
+    public $with = ['causer'];
+    
     public static function activity(string $logName = null): ActivityLogger
     {
         $defaultLogName = config('activitylog.default_log_name');
@@ -41,5 +43,17 @@ class Audit extends Activity
     public static function getActivity()
     {
         return self::all()->sortByDesc('created_at');
+    }
+
+    public function getTitle()
+    {
+        try{
+            $title = (new $this->subject_type)->collectionDetails()->singular;
+        }catch(\Exception $e)
+        {
+            $title = $this->subject_type;
+        }
+
+        return $title;
     }
 }
