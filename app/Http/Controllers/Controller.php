484c61294147c53ff8172c0d3460698fa97dbea2
Philippe Damen <philippe@thinktomorrow.be>
2018-11-16 10:53:38 +0100

    fix pagination on pages index

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,10 @@
 
 namespace Thinktomorrow\Chief\App\Http\Controllers;
 
+use Illuminate\Support\Collection;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -10,4 +13,19 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function lengthAwarePaginator(Collection $collection, $itemsPerPage, $pageName)
+    {
+        $paginator = new LengthAwarePaginator(
+            $collection->forPage(Paginator::resolveCurrentPage($pageName), $itemsPerPage),
+            $collection->count(),
+            $itemsPerPage,
+            Paginator::resolveCurrentPage($pageName),
+            [
+                'path'     => Paginator::resolveCurrentPath($pageName),
+                'pageName' => $pageName
+            ]);
+
+        return $paginator;
+    }
 }
