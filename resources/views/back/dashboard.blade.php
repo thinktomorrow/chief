5e339f9f1f9efef0c9898f0c97169841bc85ea93
Philippe Damen <philippe@thinktomorrow.be>
2019-01-11 11:31:37 +0100

    don't show modules on dashboard

--- resources/views/back/dashboard.blade.php
+++ resources/views/back/dashboard.blade.php
@@ -15,7 +15,7 @@
             <p>Don't try to follow trends. Create them</p>
         </div>
         <div class="gutter column-8 inset right">
-            @foreach(app(\Thinktomorrow\Chief\Management\Managers::class)->findByTag(['page', 'module']) as $manager)
+            @foreach(app(\Thinktomorrow\Chief\Management\Managers::class)->findByTag(['page']) as $manager)
 
                 <div class="column-6">
                     <div class="panel panel-default --raised">
