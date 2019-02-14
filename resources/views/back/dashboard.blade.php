2049f53051357d91e10ebcd81d8a273d71653757
Ben Cavens <cavensben@gmail.com>
2019-02-14 11:03:36 +0100

    Display custom managers on dashboard by tag 'dashboard'

--- resources/views/back/dashboard.blade.php
+++ resources/views/back/dashboard.blade.php
@@ -15,22 +15,24 @@
             <p>Don't try to follow trends. Create them</p>
         </div>
         <div class="gutter column-8 inset right">
-            @foreach(app(\Thinktomorrow\Chief\Management\Managers::class)->findByTag(['page']) as $manager)
+            @foreach(app(\Thinktomorrow\Chief\Management\Managers::class)->findByTag(['page', 'dashboard']) as $manager)
 
-                <div class="column-6">
-                    <div class="panel panel-default --raised">
-                        <div class="panel-body inset">
-                            <div class="btn btn-o-primary btn-circle">
-                                <i class="icon icon-box"></i>
-                            </div>
-                            <div class="stack">
-                                <h1 class="--remove-margin">{{ $manager->findAllManaged()->count() }}</h1>
-                                <p>{{ $manager->details()->plural }}</p>
-                                <a class="btn btn-secondary" href="{{ $manager->route('index') }}">Ga naar {{ $manager->details()->plural }}</a>
+                @if($manager->findAllManaged()->count() > 0)
+                    <div class="column-6">
+                        <div class="panel panel-default --raised">
+                            <div class="panel-body inset">
+                                <div class="btn btn-o-primary btn-circle">
+                                    <i class="icon icon-box"></i>
+                                </div>
+                                <div class="stack">
+                                    <h1 class="--remove-margin">{{ $manager->findAllManaged()->count() }}</h1>
+                                    <p>{{ $manager->details()->plural }}</p>
+                                    <a class="btn btn-secondary" href="{{ $manager->route('index') }}">Ga naar {{ $manager->details()->plural }}</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                @endif
 
             @endforeach
         </div>
