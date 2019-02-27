1648e372e8eb5e4111daec0863afe297b364aee8
Ben Cavens <cavensben@gmail.com>
2019-02-27 15:54:32 +0100

    Fix: avoid failure when menu title of page is not present in locale

--- src/Pages/Page.php
+++ src/Pages/Page.php
@@ -172,7 +172,7 @@ class Page extends Model implements TranslatableContract, HasMedia, ActsAsParent
 
     public function menuLabel(): string
     {
-        return $this->title;
+        return $this->title ?? '';
     }
 
     public function view()
