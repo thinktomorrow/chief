a68d9fa114fb6b61e986d8c979f0350e9c597fb7
Philippe Damen <mr.deleeuw@gmail.com>
2019-04-08 09:27:40 +0000

    Apply fixes from StyleCI

--- src/Fields/ImageRequiredValidator.php
+++ src/Fields/ImageRequiredValidator.php
@@ -7,11 +7,13 @@ class ImageRequiredValidator
     public function validate($attribute, $values, $params, $validator)
     {
         $new = isset($values['new']) ? count($values['new']) : 0;
-            $replace = isset($values['replace']) ? count($values['replace']) : 0;
-            $delete = isset($values['delete']) ? count($values['delete']) : 0;
+        $replace = isset($values['replace']) ? count($values['replace']) : 0;
+        $delete = isset($values['delete']) ? count($values['delete']) : 0;
 
-            if($new + $replace - $delete > 0) return true;
+        if ($new + $replace - $delete > 0) {
+            return true;
+        }
 
-            return false;
+        return false;
     }
 }
