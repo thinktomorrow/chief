a9e8fc121d075d6438b46364858d16958fe7d79b
Philippe Damen <philippe@thinktomorrow.be>
2019-08-20 14:59:34 +0200

    fix datefield initialization + buggy behaviour

--- tests/Feature/HealthMonitor/MonitorTest.php
+++ tests/Feature/HealthMonitor/MonitorTest.php
@@ -22,7 +22,7 @@ class MonitorTest extends TestCase
     {
         factory(Page::class)->create();
         Monitor::check();
-        $this->assertEquals('Het lijkt erop dat de homepagina niet meer bereikbaar is. <a href="http://localhost/admin/settings" class="text-secondary-800 underline hover:text-white">Kies een nieuwe</a>.', session('alertbarmessage'));
+        $this->assertEquals('Het lijkt erop dat er geen homepagina ingesteld is. Stel er een in hier: <a href="http://localhost/admin/settings" class="text-secondary-800 underline hover:text-white">Settings</a>', session('alertbarmessage'));
 
         // Now set a homepage
         Setting::create(['key' => Setting::HOMEPAGE, 'value' => 'Thinktomorrow\Chief\Pages\Page@1']);
