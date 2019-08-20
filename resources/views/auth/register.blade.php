04c21c5cfd43b0bdadc89a50222bf47d6d35ec35
KingJohnny <johnny@thinktomorrow.be>
2019-08-20 14:12:03 +0200

    There is no think tomorrow anymore

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,7 +19,7 @@
                     <div>
             
                         @if($errors and count($errors) > 0)
-                            <div class="label label-error stack">
+                            <div class="label label-warning stack">
                                 @foreach($errors->all() as $error)
                                     {{ $error }}<br>
                                 @endforeach
