98218becca293eca184b61c93fd36dcdcb908ee1
Ben Cavens <cavensben@gmail.com>
2019-08-21 09:54:49 +0200

    Move state conversions to existing migration file

--- database/migrations/2019_09_01_163503_add_page_states.php
+++ database/migrations/2019_09_01_163503_add_page_states.php
@@ -1,5 +1,6 @@
 <?php
 
+use Thinktomorrow\Chief\Pages\Page;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -11,6 +12,32 @@ class AddPageStates extends Migration
         Schema::table('pages', function (Blueprint $table) {
             $table->string('current_state')->after('morph_key')->default(\Thinktomorrow\Chief\States\PageState::DRAFT);
         });
+
+        $this->convertOldStates();
+
+        Schema::table('pages', function(Blueprint $table){
+            $table->dropColumn('archived_at');
+            $table->dropColumn('published');
+            $table->dropColumn('publication');
+        });
+    }
+
+    private function convertOldStates()
+    {
+        foreach(Page::withoutGlobalScopes()->get() as $page)
+        {
+            if($page->archived_at != null)
+            {
+                $page->state = 'archived';
+
+            }elseif($page->published)
+            {
+                $page->state = 'published';
+            }else{
+                $page->state = 'draft';
+            }
+            $page->save();
+        }
     }
 
     public function down()
