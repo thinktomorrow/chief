5c5103c76f32b450cd6aaa86f27a4b287cd9bb5b
Philippe Damen <philippe@thinktomorrow.be>
2019-10-21 10:51:06 +0200

    removed ddd helpers fixed remaining str_singular to Str::singular

--- src/Common/helpers.php
+++ src/Common/helpers.php
@@ -270,19 +270,6 @@ if (!function_exists('addQueryToUrl')) {
     }
 }
 
-if (!function_exists('ddd')) {
-    function ddd($var, ...$moreVars)
-    {
-        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1);
-        if (php_sapi_name() == 'cli') {
-            print_r("\e[1;30m dumped at: " . str_replace(base_path(), '', $trace[0]['file']). ", line: " . $trace[0]['line'] . "\e[40m\n");
-        } else {
-            print_r("[dumped at: " . str_replace(base_path(), '', $trace[0]['file']). ", line: " . $trace[0]['line'] . "]\n");
-        }
-        return dd($var, ...$moreVars);
-    }
-}
-
 if (!function_exists('chiefMemoize')) {
     /**
      * Memoize a function
