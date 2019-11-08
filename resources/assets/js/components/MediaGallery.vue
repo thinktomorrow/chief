53d984ebbca97e71c528fe4873d9ee91b19bc8bc
Philippe Damen <philippe@thinktomorrow.be>
2019-11-08 15:54:34 +0100

    make mediagallery popup scrollable

--- resources/assets/js/components/MediaGallery.vue
+++ resources/assets/js/components/MediaGallery.vue
@@ -1,6 +1,6 @@
 <template>
 	<modal :id="id" class="large-modal" type="modal">
-		<div class="gutter-s row overflow-scroll">
+		<div class="gutter-s row overflow-auto" style="height: 400px">
 			<div v-if="isLoading && assets.length < 1">
 				<svg width="150px"  height="150px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-dual-ring" style="background: none;"><circle cx="50" cy="50" fill="none" stroke-linecap="round" r="40" stroke-width="10" stroke="#5C4456" stroke-dasharray="62.83185307179586 62.83185307179586" transform="rotate(233.955 50 50)"><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animateTransform></circle></svg>
 			</div>
