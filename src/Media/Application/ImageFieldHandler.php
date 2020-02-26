ba29e6a06dc10094546b2b9807ab70f8fc0ec455
Philippe Damen <philippe@thinktomorrow.be>
2020-02-26 14:16:21 +0100

    fix new upload as existing after removing item in slim

--- src/Media/Application/ImageFieldHandler.php
+++ src/Media/Application/ImageFieldHandler.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Thinktomorrow\Chief\Media\Application;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
 use Thinktomorrow\AssetLibrary\Asset;
 use Thinktomorrow\AssetLibrary\HasAsset;
 use Thinktomorrow\Chief\Fields\Types\MediaField;
@@ -44,10 +43,18 @@ class ImageFieldHandler extends AbstractMediaFieldHandler
 
     protected function createNewAsset(HasAsset $model, string $locale, string $type, $value): Asset
     {
-        // Inputted value is expected to be a slim specific json string.
-        $file = json_decode($value)->output->image;
+        $value = json_decode($value);
 
-        $filename = json_decode($value)->output->name;
+        // Slim can sometimes sent us the ajax upload response instead of the asset id. Let's make sure this is being dealt with.
+        if (isset($value->id) && $this->looksLikeAnAssetId($value->id)) {
+            trap('goth ere');
+            return $this->newExistingAsset($model, $locale, $type, $value->id);
+        }
+
+        // Inputted value is expected to be a slim specific json string.
+        $file = $value->output->image;
+
+        $filename = $value->output->name;
 
         return $this->assetUploader->uploadFromBase64($file, $filename);
     }
