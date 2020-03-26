414839449e45c37abe7d4539836d4cf657e64d98
Ben Cavens <cavensben@gmail.com>
2020-03-26 07:21:32 +0100

    Fix: don't provide new target on archive when model doesn't provide url

--- resources/views/back/managers/_modals/archive-modal.blade.php
+++ resources/views/back/managers/_modals/archive-modal.blade.php
@@ -5,28 +5,34 @@
         <form action="{{ $manager->assistant('archive')->route('archive') }}" method="POST" id="archive-manager-form-{{ $managedModelId }}">
             @csrf
             <div v-cloak>
-                <h2 class="formgroup-label">Archiveer de pagina: {{ $manager->details()->title }}</h2>
-                <p>
-                    Opgelet, dit haalt deze pagina van de site en bezoekers krijgen een 404-pagina te zien.<br>
-                    Je kan ook kiezen om door te linken naar een andere pagina. Stel die pagina hieronder in.
-                </p>
-                <div class="input-group stack column-6">
-                    <chief-multiselect
-                            name="redirect_id"
-                            :options='@json($targetModels)'
-                            selected='@json(old('redirect_id'))'
-                            grouplabel="group"
-                            groupvalues="values"
-                            labelkey="label"
-                            valuekey="id"
-                    >
-                    </chief-multiselect>
+                <h2 class="formgroup-label">Archiveer: {{ $manager->details()->title }}</h2>
+                @if(contract($manager->existingModel(), \Thinktomorrow\Chief\Urls\ProvidesUrl\ProvidesUrl::class))
+                    <p>
+                        Opgelet, dit haalt deze pagina van de site en bezoekers krijgen een 404-pagina te zien.<br>
+                        Je kan ook kiezen om door te linken naar een andere pagina. Stel die pagina hieronder in.
+                    </p>
+                    <div class="input-group stack column-6">
+                        <chief-multiselect
+                                name="redirect_id"
+                                :options='@json($targetModels)'
+                                selected='@json(old('redirect_id'))'
+                                grouplabel="group"
+                                groupvalues="values"
+                                labelkey="label"
+                                valuekey="id"
+                        >
+                        </chief-multiselect>
+                    @else
+                        <p>
+                            Archiveren haalt de {{ $manager->existingModel()->labelSingular }} onmiddellijk van de site.<br>
+                        </p>
+                    @endif
                 </div>
             </div>
         </form>
 
         <div v-cloak slot="modal-action-buttons">
-            <button type="button" class="btn btn-link" data-submit-form="archive-manager-form-{{ $managedModelId }}">Archiveer</button>
+            <button type="button" class="btn btn-primary" data-submit-form="archive-manager-form-{{ $managedModelId }}">Archiveer</button>
         </div>
     </modal>
 @endpush
