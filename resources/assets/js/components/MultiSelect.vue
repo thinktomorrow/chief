ce25310362d11ab777d883f6913c2fe3f568ed20
Philippe Damen <philippe@thinktomorrow.be>
2020-04-09 13:44:45 +0200

    fix multiselect empty object options

--- resources/assets/js/components/MultiSelect.vue
+++ resources/assets/js/components/MultiSelect.vue
@@ -139,8 +139,7 @@
                 options = this.isJson(options) ? JSON.parse(options) : options;
 
                 // We need an array so if options is given as key:value pairs, we convert them here.
-                if(this.isKeyValuePair(options) && options.length > 0) {
-
+                if(this.isKeyValuePair(options) && (options.length > 0 || Object.keys(options).length > 0)) {
                     let convertedOptions = [];
 
                     Object.getOwnPropertyNames(options).forEach((key) => {
