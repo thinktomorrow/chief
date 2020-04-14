ba7d5cba42e714e48d0261433badd66ff6cc175b
Ben Cavens <cavensben@gmail.com>
2020-04-14 13:36:59 +0200

    Fix: not all page collections are paginated

--- resources/views/back/dashboard.blade.php
+++ resources/views/back/dashboard.blade.php
@@ -19,13 +19,15 @@
 
                 @if(!$manager->can('index')) @continue @endif
 
-                @if($manager->indexCollection()->total() > 0)
+                <?php $total = method_exists($manager->indexCollection(), 'total') ? $manager->indexCollection()->total() : $manager->indexCollection()->count(); ?>
+
+                @if($total > 0)
                     <div class="column-6">
                         <div class="rounded bg-white shadow">
                             <div class="inset">
                                 <div class="flex items-center mb-4">
-                                    <span class="text-4xl font-bold inline-block mr-4" style="leading-none">{{ $manager->indexCollection()->total() }}</span>
-                                    <p class="mt-2 text-lg">{{ $manager->indexCollection()->total() == 1 ? $manager->details()->singular : $manager->details()->plural }}</p>
+                                    <span class="text-4xl font-bold inline-block mr-4" style="leading-none">{{ $total }}</span>
+                                    <p class="mt-2 text-lg">{{ $total == 1 ? $manager->details()->singular : $manager->details()->plural }}</p>
                                 </div>
                                 <a class="btn btn-primary" href="{{ $manager->route('index') }}">Ga naar {{ $manager->details()->plural }}</a>
                             </div>
