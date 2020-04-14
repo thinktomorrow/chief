c8d5d9a91d90d948b4274a735a86a83b4ae29b51
Philippe Damen <philippe@thinktomorrow.be>
2020-04-14 14:21:41 +0200

    fixing squanto redactor layout

--- resources/views/vendor/squanto/editor-script.blade.php
+++ resources/views/vendor/squanto/editor-script.blade.php
@@ -1,16 +1,21 @@
 @push('custom-scripts')
-<script>
-    $R.options = {
-        plugins: ['alignment', 'rich-links'],
-        @if(chiefAdmin()->hasRole('developer'))
-        buttons: ['html', 'format', 'bold', 'italic', 'lists', 'link'],
-        @else
-        buttons: ['format', 'bold', 'italic', 'lists', 'link'],
-        @endif
-        formatting: ['p', 'h2', 'h3'],
-        definedlinks: '{{ route('chief.api.internal-links') }}',
-    };
-</script>
+    <script src="{{ asset('assets/back/js/vendor/redactor.js') }}"></script>
+    <script>
+        $R.options = {
+            plugins: ['alignment', 'rich-links'],
+            @if(chiefAdmin()->hasRole('developer'))
+                buttons: ['html', 'format', 'bold', 'italic', 'lists', 'link'],
+            @else
+            buttons: ['format', 'bold', 'italic', 'lists', 'link'],
+            @endif
+            formatting: ['p', 'h2', 'h3'],
+            definedlinks: '{{ route('chief.api.internal-links') }}',
+        };
+    </script>
+@endpush
+
+@push('custom-styles')
+    <link rel="stylesheet" href="{{ asset('assets/back/css/vendor/redactor.css') }}">
 @endpush
 
 <script>
