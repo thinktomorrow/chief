46bbec76576c00f826342c79f17c92af77b02cd4
Ben Cavens <cavensben@gmail.com>
2020-08-21 17:08:38 +0200

    Add media gallery to site menu

--- resources/views/back/_layouts/_partials/nav.blade.php
+++ resources/views/back/_layouts/_partials/nav.blade.php
@@ -15,10 +15,11 @@
                 @if(\Illuminate\Support\Facades\Gate::check('update-page') || \Illuminate\Support\Facades\Gate::check('view-page') || \Illuminate\Support\Facades\Gate::check('view-squanto'))
                     <li>
                         <dropdown>
-                            <span class="center-y {{ (isActiveUrl('admin/translations*') || isActiveUrl('admin/menu*')) ? 'active' : '' }}" slot="trigger" slot-scope="{ toggle, isActive }" @click="toggle">Site</span>
+                            <span class="center-y {{ (isActiveUrl('admin/translations*') || isActiveUrl('admin/mediagallery*') || isActiveUrl('admin/menus*') || isActiveUrl('admin/sitemap*')) ? 'active' : '' }}" slot="trigger" slot-scope="{ toggle, isActive }" @click="toggle">Site</span>
                             <div v-cloak class="dropdown-box inset-s">
                                 @can('update-page')
                                     <a class="{{ isActiveUrl('admin/menus*') ? 'active' : '' }}" href="{{ route('chief.back.menus.index') }}">Menu</a>
+                                    <a class="{{ isActiveUrl('admin/mediagallery*') ? 'active' : '' }}" href="{{ route('chief.mediagallery.index') }}">Media</a>
                                 @endcan
                                 @can('view-page')
                                     @if(\Thinktomorrow\Chief\Modules\Module::atLeastOneRegistered())
