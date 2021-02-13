eb31ef40e45ab2ab8f10e963ec6fccdfaa4d5470
Ben Cavens <cavensben@gmail.com>
2021-02-13 15:07:24 +0100

    Fix links vue comps

--- resources/views/managers/links.blade.php
+++ resources/views/managers/links.blade.php
@@ -1,7 +1,7 @@
 @foreach($linkForm->links() as $locale => $links)
     <h3>{{ $locale }}</h3>
     <div>CURRENT: {{ optional($links->current)->slug }}</div>
-    <div>
+    <div data-vue-fields>
         @foreach($links->redirects as $urlRecord)
             <url-redirect inline-template removeurl="{{ route('chief.back.assistants.url.remove-redirect',$urlRecord->id) }}">
                 <div v-show="!this.removed" class="bg-white panel inset-s stack-s">
@@ -20,85 +20,25 @@
     @csrf
     @method('PUT')
 
-    @foreach($linkForm->formValues() as $locale => $formValues)
+    <div data-vue-fields>
+        @foreach($linkForm->formValues() as $locale => $formValues)
 
-        <link-input inline-template
-                    checkurl="{{ route('chief.back.links.check') }}"
-                    initial-value="{{ old('links.'.$locale, $formValues->value) }}"
-                    fixed-segment="{{ $formValues->fixedSegment }}"
-                    model-class="{{ get_class($model) }}"
-                    model-id="{{ $model->id }}" >
-            <div>
-                <label for="links.{{ $locale }}">{{ $locale }}</label>
-                <span>fixed segment: <span v-text="fixedSegment"></span></span>
-                <input @keyup="onInput" id="links.{{ $locale }}" class="input inset-s" type="text" name="links[{{ $locale }}]" v-model="value" />
-                <span v-if="is_homepage" class="inline-block label label--primary">Homepage link</span>
-                <p class="inline-block right text-error" v-if="hint" v-html="hint"></p>
-            </div>
-        </link-input>
-    @endforeach
+            <link-input inline-template
+                        checkurl="{{ route('chief.back.links.check') }}"
+                        initial-value="{{ old('links.'.$locale, $formValues->value) }}"
+                        fixed-segment="{{ $formValues->fixedSegment }}"
+                        model-class="{{ get_class($model) }}"
+                        model-id="{{ $model->id }}" >
+                <div>
+                    <label for="links.{{ $locale }}">{{ $locale }}</label>
+                    <span>fixed segment: <span v-text="fixedSegment"></span></span>
+                    <input @keyup="onInput" id="links.{{ $locale }}" class="input inset-s" type="text" name="links[{{ $locale }}]" v-model="value" />
+                    <span v-if="is_homepage" class="inline-block label label--primary">Homepage link</span>
+                    <p class="inline-block right text-error" v-if="hint" v-html="hint"></p>
+                </div>
+            </link-input>
+        @endforeach
+    </div>
+
     <button class="btn btn-primary mt-4" type="submit">Pas aan</button>
 </form>
-
-
-@push('custom-components')
-    <script>
-        Vue.component('link-input',{
-            props: ['checkurl', 'fixedSegment', 'initialValue', 'modelClass', 'modelId'],
-            data: function(){
-                return {
-                    value: this.initialValue,
-                    is_homepage: (this.initialValue === '/'),
-                    hint: '',
-                };
-            },
-            methods: {
-                onInput: function(e){
-                    this.is_homepage = (e.target.value === '/');
-                    this._checkUniqueness(e.target.value);
-                },
-                _checkUniqueness: _.debounce(function(value){
-
-                    // An empty value is never checked for uniqueness
-                    if(!value){
-                        this.hint = '';
-                        return;
-                    }
-
-                    const completeSlug = this.fixedSegment+'/'+value;
-
-                    window.axios.post(this.checkurl, {
-                        modelClass: this.modelClass,
-                        modelId: this.modelId,
-                        slug: completeSlug.replace(/\/\//, '')
-                    }).then(({data}) => {
-                        this.hint = data.hint;
-                    });
-                }, 300)
-            }
-
-        });
-
-        Vue.component('url-redirect',{
-            props: ['fieldProp', 'removeurl'],
-            data: function(){
-                return {
-                    removed: false,
-                };
-            },
-            methods: {
-                remove: function(){
-
-                    const self = this;
-
-                    window.axios.post(this.removeurl, {
-                        _method: 'DELETE',
-                    }).then(function({data}){
-                        self.removed = true;
-                    });
-                }
-            }
-
-        });
-    </script>
-@endpush
