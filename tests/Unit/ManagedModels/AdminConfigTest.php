1bf61c1e38fe2ad97659b3de08898c5375710e34
Ben Cavens <cavensben@gmail.com>
2021-04-13 22:07:59 +0200

    Fix: test

---
+++ tests/Unit/ManagedModels/AdminConfigTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thinktomorrow\Chief\Tests\Unit\ManagedModels;
+
+use Thinktomorrow\Chief\Admin\AdminConfig;
+use Thinktomorrow\Chief\Tests\Shared\Fakes\ArticlePage;
+use Thinktomorrow\Chief\Tests\TestCase;
+
+class AdminConfigTest extends TestCase
+{
+    /** @test */
+    public function it_contains_values_for_admin_views()
+    {
+        $config = AdminConfig::make();
+
+        $config->paginate(2);
+        $config->indexTitle('index_title');
+        $config->pageTitle('page_title');
+        $config->navTitle('nav_title');
+        $config->modelName('model_name');
+        $config->rowContent('row_content');
+        $config->rowTitle('row_title');
+        $config->rowBadge('row_badge');
+
+        $this->assertEquals(2, $config->getPagination());
+        $this->assertEquals('index_title', $config->getIndexTitle());
+        $this->assertEquals('page_title', $config->getPageTitle());
+        $this->assertEquals('nav_title', $config->getNavTitle());
+        $this->assertEquals('model_name', $config->getModelName());
+        $this->assertEquals('row_content', $config->getRowContent());
+        $this->assertEquals('row_title', $config->getRowTitle());
+        $this->assertEquals('row_badge', $config->getRowBadge());
+    }
+
+    /** @test */
+    public function it_returns_defaults_for_model()
+    {
+        $model = ArticlePage::make([
+            'title' => 'first article',
+        ]);
+
+        /** @var AdminConfig $config */
+        $config = $model->adminConfig();
+
+        $this->assertEquals('ArticlePages', $config->getIndexTitle());
+        $this->assertEquals('first article', $config->getPageTitle());
+        $this->assertEquals('ArticlePage', $config->getModelName());
+    }
+}
