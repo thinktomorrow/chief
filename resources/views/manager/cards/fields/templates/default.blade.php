5cf725f960d18fd5363a85acd4a829a99178532e
Ben Cavens <cavensben@gmail.com>
2021-04-26 17:10:27 +0200

    Show default fields

--- resources/views/manager/cards/fields/templates/default.blade.php
+++ resources/views/manager/cards/fields/templates/default.blade.php
@@ -1,36 +1,38 @@
-<x-chief-card
-    class="{{ isset($class) ? $class : '' }}"
-    title="{{ $title ?? null }}"
-    :editRequestUrl="$manager->route('fields-edit', $model, $componentKey)"
-    type="fields-{{ $componentKey }}"
->
-    <div class="space-y-6">
-        @foreach($fields as $field)
-            <div class="space-y-2">
-                <span class="text-sm uppercase font-semibold text-grey-500 tracking-widest">{{ $field->getLabel() }}</span>
+@if(count($fields) > 0)
+    <x-chief-card
+            class="{{ isset($class) ? $class : '' }}"
+            title="{{ $title ?? null }}"
+            :editRequestUrl="$manager->route('fields-edit', $model, $componentKey)"
+            type="fields-{{ $componentKey }}"
+    >
+        <div class="space-y-6">
+            @foreach($fields as $field)
+                <div class="space-y-2">
+                    <span class="text-sm uppercase font-semibold text-grey-500 tracking-widest">{{ $field->getLabel() }}</span>
 
-                <!-- off course, this should be sensible, for all kinds of fieldtypes, checkboxes, images, ... ... -->
-                <!-- as well as localisation ... -->
+                    <!-- off course, this should be sensible, for all kinds of fieldtypes, checkboxes, images, ... ... -->
+                    <!-- as well as localisation ... -->
 
-                {{-- Todo: this condition doesn't work for example for ImageField, as it's value is null --}}
-                @if($field->getValue())
-                    <div class="prose prose-dark">
-                        @switch(get_class($field))
-                            @case(\Thinktomorrow\Chief\ManagedModels\Fields\Types\InputField::class)
+                    {{-- Todo: this condition doesn't work for example for ImageField, as it's value is null --}}
+                    @if($field->getValue())
+                        <div class="prose prose-dark">
+                            @switch(get_class($field))
+                                @case(\Thinktomorrow\Chief\ManagedModels\Fields\Types\InputField::class)
                                 <p>{{ $field->getValue() }}</p>
                                 @break
-                            @case(\Thinktomorrow\Chief\ManagedModels\Fields\Types\MediaField::class)
+                                @case(\Thinktomorrow\Chief\ManagedModels\Fields\Types\MediaField::class)
                                 <img src="{{ $field->getValue() }}" alt="image">
                                 @break
-                            @default
+                                @default
                                 {!! $field->getValue() !!}
                                 @break
-                        @endswitch
-                    </div>
-                @else
-                    <p>...</p>
-                @endif
-            </div>
-        @endforeach
-    </div>
-</x-chief-card>
+                            @endswitch
+                        </div>
+                    @else
+                        <p>...</p>
+                    @endif
+                </div>
+            @endforeach
+        </div>
+    </x-chief-card>
+@endif
