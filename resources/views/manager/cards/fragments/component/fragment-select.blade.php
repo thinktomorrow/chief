52d44abc8b94613882de8aa74b063352cc5ebfa8
Ben Cavens <cavensben@gmail.com>
2021-06-28 14:31:38 +0200

    Show default add when no fragments

--- resources/views/manager/cards/fragments/component/fragment-select.blade.php
+++ resources/views/manager/cards/fragments/component/fragment-select.blade.php
@@ -1,28 +1,34 @@
+<?php $showOpenState = !isset($inOpenState) || !$inOpenState; ?>
+
 <div data-fragment-select
      data-sortable-ignore
      class="relative w-full">
 
     <!-- plus icon -->
-    <div data-fragment-select-open
-         class="absolute flex justify-center z-1 border-none group w-full h-8 cursor-pointer"
-         style="margin-top: -12px;"
-    >
-        <div class="absolute link link-black bg-white rounded-full transition-150 transform scale-0 group-hover:scale-100">
-            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                 stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
-            </svg>
+    @if($showOpenState)
+        <div data-fragment-select-open
+             class="absolute flex justify-center z-1 border-none group w-full h-8 cursor-pointer"
+             style="margin-top: -12px;"
+        >
+            <div class="absolute link link-black bg-white rounded-full transition-150 transform scale-0 group-hover:scale-100">
+                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                     stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                </svg>
+            </div>
         </div>
-    </div>
+    @endif
 
     <!-- select options: create new or add existing -->
     <div data-fragment-select-options
-         class="hidden relative p-6 pop border-t border-grey-100"
+         class="{{ ($showOpenState) ? 'hidden' : '' }} relative p-6 pop border-t border-grey-100"
     >
-        <a data-fragment-select-close class="absolute top-0 right-0 m-6 cursor-pointer link link-grey">
-            <x-icon-label type="close"></x-icon-label>
-        </a>
+        @if($showOpenState)
+            <a data-fragment-select-close class="absolute top-0 right-0 m-6 cursor-pointer link link-grey">
+                <x-icon-label type="close"></x-icon-label>
+            </a>
+        @endif
 
         <div class="flex items-center justify-center w-full gutter-2">
             <div>
--- resources/views/manager/cards/fragments/component/fragments-main.blade.php
+++ resources/views/manager/cards/fragments/component/fragments-main.blade.php
@@ -1,24 +1,27 @@
 <div data-fragments-component>
-    <div
-        data-fragments-container
-        data-sortable-fragments
-        data-sortable-endpoint="@adminRoute('fragments-reorder', $owner)"
-        class="relative divide-y divide-grey-100 border-t border-b border-grey-100 -m-8"
-    >
+    <div class="relative divide-y divide-grey-100 border-t border-b border-grey-100 -m-8">
 
         @include('chief::manager.cards.fragments.component.fragment-select', [
             'ownerManager' => $manager,
+            'inOpenState' => count($fragments) < 1
         ])
 
-        @foreach($fragments as $fragment)
-            @include('chief::manager.cards.fragments.component._card', [
-                'model' => $fragment['model'],
-                'manager' => $fragment['manager'],
-                'owner' => $owner,
-                'ownerManager' => $manager,
-                'loop' => $loop,
-            ])
-        @endforeach
+        <div data-fragments-container
+             data-sortable-fragments
+             data-sortable-endpoint="@adminRoute('fragments-reorder', $owner)"
+             class="divide-y divide-grey-100">
+            @foreach($fragments as $fragment)
+                @include('chief::manager.cards.fragments.component._card', [
+                    'model' => $fragment['model'],
+                    'manager' => $fragment['manager'],
+                    'owner' => $owner,
+                    'ownerManager' => $manager,
+                    'loop' => $loop,
+                ])
+            @endforeach
+        </div>
+
+
     </div>
 
 {{--    @include('chief::manager.cards.fragments.component.fragment-select', [--}}
