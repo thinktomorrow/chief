0532f590c4068c08a5dae129e5962b602cfe0534
TijsTT <tijs@thinktomorrow.be>
2021-06-28 15:20:51 +0200

    Change: fragment select new ui

--- resources/views/manager/cards/fragments/component/fragment-select.blade.php
+++ resources/views/manager/cards/fragments/component/fragment-select.blade.php
@@ -4,50 +4,45 @@
 
     <!-- plus icon -->
     <div data-fragment-select-open
-         class="absolute flex justify-center z-1 border-none group w-full h-8 cursor-pointer"
+         class="absolute flex justify-center w-full h-8 border-none cursor-pointer z-1 group"
          style="margin-top: -12px;"
     >
-        <div class="absolute link link-black bg-white rounded-full transition-150 transform scale-0 group-hover:scale-100">
-            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                 stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
-            </svg>
+        <div class="absolute transform scale-0 bg-white rounded-full link link-black transition-150 group-hover:scale-100">
+            <svg width="24" height="24"> <use xlink:href="#icon-add-circle"/> </svg>
         </div>
     </div>
 
     <!-- select options: create new or add existing -->
-    <div data-fragment-select-options
-         class="hidden relative p-6 pop border-t border-grey-100"
+    <div
+        data-fragment-select-options
+        class="relative hidden p-6 border-t pop border-grey-100"
     >
-        <a data-fragment-select-close class="absolute top-0 right-0 m-6 cursor-pointer link link-grey">
+        <a data-fragment-select-close class="absolute top-0 right-0 m-6 cursor-pointer link link-primary">
             <x-icon-label type="close"></x-icon-label>
         </a>
 
-        <div class="flex items-center justify-center w-full gutter-2">
+        <div class="flex items-center justify-center gutter-3">
             <div>
-                <a data-sidebar-trigger="selectFragment"
-                   href="{{ $ownerManager->route('fragments-select-new', $owner) }}"
-                   class="flex flex-col items-center justify-center space-y-1 label label-primary label-xl"
+                <a
+                    data-sidebar-trigger="selectFragment"
+                    href="{{ $ownerManager->route('fragments-select-new', $owner) }}"
+                    class="flex flex-col items-center justify-center space-y-1 label label-primary label-xl hover:bg-primary-500 hover:text-white transition-75"
                 >
-                    <svg width="24" height="24">
-                        <use xlink:href="#icon-add"/>
-                    </svg>
+                    <svg width="24" height="24"> <use xlink:href="#icon-document-add"/> </svg>
 
-                    <span>Maak een nieuw fragment</span>
+                    <span class="font-semibold">Nieuw fragment</span>
                 </a>
             </div>
 
             <div>
-                <a data-sidebar-trigger="selectFragment"
-                   href="{{ $ownerManager->route('fragments-select-existing', $owner) }}"
-                   class="flex flex-col items-center justify-center space-y-1 label label-primary label-xl"
+                <a
+                    data-sidebar-trigger="selectFragment"
+                    href="{{ $ownerManager->route('fragments-select-existing', $owner) }}"
+                    class="flex flex-col items-center justify-center space-y-1 label label-primary label-xl hover:bg-primary-500 hover:text-white transition-75"
                 >
-                    <svg width="24" height="24">
-                        <use xlink:href="#icon-duplicate"/>
-                    </svg>
+                    <svg width="24" height="24"> <use xlink:href="#icon-duplicate"/> </svg>
 
-                    <span>Kies een bestaand fragment</span>
+                    <span class="font-semibold">Bestaand fragment</span>
                 </a>
             </div>
         </div>
--- resources/views/wireframes/wireframe.blade.php
+++ resources/views/wireframes/wireframe.blade.php
@@ -3,7 +3,7 @@
         data-wireframe="{{ $name }}"
         data-wireframe-css="{{ $css }}"
     @endif
-    class="p-4 rounded-xl border border-grey-200 bg-grey-50 {{ $attributes->get('class') }}"
+    class="p-4 rounded-xl bg-grey-100 {{ $attributes->get('class') }}"
     style="{{ $attributes->get('style') }}"
 >
     {{ $slot }}
