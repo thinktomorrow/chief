809fa75e5b1bd4a06b855843102a6daf14675e04
Ben Cavens <cavensben@gmail.com>
2021-06-29 14:53:52 +0200

    Fix: selected value in select card

--- resources/views/manager/cards/fields/templates/types/select.blade.php
+++ resources/views/manager/cards/fields/templates/types/select.blade.php
@@ -1,11 +1,11 @@
-@if($field->getValue())
-    @php
-        $values = gettype($field->getValue()) == 'array' ? $field->getValue() : (array) $field->getValue();
-    @endphp
+@php
+    $selected =  (array) $field->getSelected() ?? $field->getValue($locale ?? null);
+@endphp
 
+@if(count($selected) > 0)
     <div class="flex flex-wrap -m-0.5">
         @foreach($field->getOptions() as $optionKey => $optionValue)
-            @if(in_array($optionKey, $values))
+            @if(in_array($optionKey, $selected))
                 <div class="p-0.5">
                     <span class="inline-block label label-info">{{ $optionValue }}</span>
                 </div>
