5cd6f9c4c52e83fef27a2f78b441cf6da60cd649
Ben Cavens <cavensben@gmail.com>
2021-07-01 13:15:55 +0200

    eslint to the rescue

--- resources/assets/js/utilities/character-count.js
+++ resources/assets/js/utilities/character-count.js
@@ -1,23 +1,23 @@
 import EventBus from './EventBus';
 
 function characterCount(panel, characterCountEl) {
-    const formFieldId = characterCountEl.getAttribute('data-character-count'),
-        formField = panel.querySelector('#' + (formFieldId.replaceAll('.','\\.'))),
-        max = characterCountEl.getAttribute('data-character-count-max');
+    const formFieldId = characterCountEl.getAttribute('data-character-count');
+    const formField = panel.querySelector(`#${formFieldId.replaceAll('.', '\\.')}`);
+    const max = characterCountEl.getAttribute('data-character-count-max');
 
-    if(!formField) {
-        console.error('Character count not initiated: No formField found by selector: ' + '#' + formFieldId);
+    if (!formField) {
+        console.error(`Character count not initiated: No formField found by selector:#${formFieldId}`);
         return;
     }
 
     formField.addEventListener('input', function () {
         const currentLength = this.value.length;
 
-        characterCountEl.classList.remove('text-red-400','text-orange-400');
+        characterCountEl.classList.remove('text-red-400', 'text-orange-400');
 
         if (currentLength >= max) {
             characterCountEl.classList.add('text-red-400');
-        } else if(currentLength >= (max - (max * 0.1))) {
+        } else if (currentLength >= (max - (max * 0.1))) {
             characterCountEl.classList.add('text-orange-400');
         }
 
@@ -28,10 +28,10 @@ function characterCount(panel, characterCountEl) {
     characterCountEl.innerHTML = formField.value.length;
 }
 
-EventBus.subscribe('sidebarPanelActivated', function (data) {
+EventBus.subscribe('sidebarPanelActivated', (data) => {
     //     const characterCountEl = container.querySelector('[data-character-count=' + id + ']');
 
-    data.panel.el.querySelectorAll('[data-character-count]').forEach(function(el){
+    data.panel.el.querySelectorAll('[data-character-count]').forEach((el) => {
         console.log(el);
         characterCount(data.panel.el, el);
     });
