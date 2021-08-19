0cfbe665135fd8fb5f251a124a72ad3fc949ff22
Ben Cavens <cavensben@gmail.com>
2021-08-19 20:33:48 +0200

    Fix: menu render in admin

--- resources/views/admin/menu/_partials/menu-item.blade.php
+++ resources/views/admin/menu/_partials/menu-item.blade.php
@@ -9,41 +9,41 @@
     <div class="flex items-start justify-between w-full space-x-4" style="padding-left: {{ $level * 2 }}rem; {{ $level > 0 ? 'margin-left: 0.75rem;' : null }}">
         <div class="flex flex-wrap items-center">
             <div class="mr-3 space-x-1">
-                <a href="{{ route('chief.back.menuitem.edit', $item->id) }}">
-                    <span class="font-medium text-grey-700">{{ $item->label }}</span>
+                <a href="{{ route('chief.back.menuitem.edit', $item->getId()) }}">
+                    <span class="font-medium text-grey-700">{{ $item->getLabel() }}</span>
                 </a>
 
                 {{-- TODO: what is this? --}}
-                @if($item->hidden_in_menu)
+                @if($item->isHiddenInMenu())
                     <span class="text-grey-500">(Online, maar verborgen in het menu)</span>
-                @elseif($item->draft)
+                @elseif($item->isDraft())
                     <span class="text-grey-500">(Offline)</span>
                 @endif
             </div>
 
-            @if($item->type == \Thinktomorrow\Chief\Site\Menu\MenuItem::TYPE_INTERNAL)
+            @if($item->getType() == \Thinktomorrow\Chief\Site\Menu\MenuItem::TYPE_INTERNAL)
                 <x-icon-label type="forward" class="mr-3 text-primary-500"></x-icon-label>
 
-                <a class="inline-block label label-primary" href="{{ $item->url }}" target="_blank">
+                <a class="inline-block label label-primary" href="{{ $item->getUrl() }}" target="_blank">
                     {{-- Todo: show internal page title --}}
-                    {{ $item->page_label }}
+                    {{ $item->getPageLabel() }}
                 </a>
-            @elseif($item->type == \Thinktomorrow\Chief\Site\Menu\MenuItem::TYPE_NOLINK)
+            @elseif($item->getType() == \Thinktomorrow\Chief\Site\Menu\MenuItem::TYPE_NOLINK)
                 <x-icon-label type="forward" class="mr-3 text-grey-300"></x-icon-label>
 
                 <span class="font-medium text-grey-300">Geen link</span>
             @else
                 <x-icon-label type="forward" class="mr-3 text-primary-500"></x-icon-label>
 
-                <a class="link link-primary" href="{{ $item->url }}" target="_blank">
-                    {{ $item->url }}
+                <a class="link link-primary" href="{{ $item->getUrl() }}" target="_blank">
+                    {{ $item->getUrl() }}
                 </a>
             @endif
         </div>
 
         <div class="flex items-center flex-shrink-0 cursor-pointer">
             <a
-                href="{{ route('chief.back.menuitem.edit', $item->id) }}"
+                href="{{ route('chief.back.menuitem.edit', $item->getId()) }}"
                 class="link link-primary"
             >
                 <x-icon-label type="edit"></x-icon-label>
@@ -52,7 +52,7 @@
     </div>
 </div>
 
-@foreach($item->children as $subItem)
+@foreach($item->getChildNodes() as $subItem)
     @include('chief::admin.menu._partials.menu-item', [
         'item' => $subItem,
         'level' => $level + 1
