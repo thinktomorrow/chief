0e939e9be10aa771a9d38a2406763c22d203a3a6
Ben Cavens <cavensben@gmail.com>
2021-09-15 22:28:31 +0200

    Fix: view grouped select in window

--- resources/views/manager/fields/window/types/select.blade.php
+++ resources/views/manager/fields/window/types/select.blade.php
@@ -6,10 +6,10 @@
     <div class="flex flex-wrap -m-0.5">
         @if($field->isGrouped())
             @foreach($field->getOptions() as $fieldGroup)
-                @foreach($fieldGroup['values'] as $optionKey => $optionValue)
-                    @if(in_array($optionKey, $selected))
+                @foreach($fieldGroup['values'] as $optionValue)
+                    @if(in_array($optionValue['id'], $selected))
                         <div class="p-0.5">
-                            <span class="inline-block label label-info">{{ $optionValue }}</span>
+                            <span class="inline-block label label-info">{{ $optionValue['label'] }}</span>
                         </div>
                     @endif
                 @endforeach
