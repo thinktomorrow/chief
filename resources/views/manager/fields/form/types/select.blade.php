e38bd5883c16bada00c075199cd724a219b39f52
Ben Cavens <cavensben@gmail.com>
2021-09-17 16:35:08 +0200

    Add: option for native Select field

--- resources/views/manager/fields/form/types/select.blade.php
+++ resources/views/manager/fields/form/types/select.blade.php
@@ -9,6 +9,15 @@
         labelkey="label"
         valuekey="id"
     ></chief-multiselect>
+@elseif($field->prefersNativeSelect())
+    <select
+            name="{{ isset($locale) ? $field->getName($locale) : $field->getName() }}"
+            id="{{ $field->getId($locale ?? null) }}">
+            <option value="">---</option>
+        @foreach($field->getOptions() as $key => $value)
+            <option {{ in_array($key, (array) old($key, $field->getSelected() ?? $field->getValue($locale ?? null))) ? 'selected' : '' }} value="{{ $key }}">{{ $value }}</option>
+        @endforeach
+    </select>
 @else
     <chief-multiselect
         name="{{ isset($locale) ? $field->getName($locale) : $field->getName() }}"
