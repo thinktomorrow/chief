6d6a059c37049a5c27f7702deaa3b81215ec88ab
TijsTT <tijs@thinktomorrow.be>
2021-09-17 16:54:24 +0200

    Change: native select field styling

--- resources/views/manager/fields/form/types/select.blade.php
+++ resources/views/manager/fields/form/types/select.blade.php
@@ -10,14 +10,26 @@
         valuekey="id"
     ></chief-multiselect>
 @elseif($field->prefersNativeSelect())
-    <select
+    <div class="relative flex items-center justify-end cursor-pointer">
+        <select
             name="{{ isset($locale) ? $field->getName($locale) : $field->getName() }}"
-            id="{{ $field->getId($locale ?? null) }}">
+            id="{{ $field->getId($locale ?? null) }}"
+            class="select"
+        >
             <option value="">---</option>
-        @foreach($field->getOptions() as $key => $value)
-            <option {{ in_array($key, (array) old($key, $field->getSelected() ?? $field->getValue($locale ?? null))) ? 'selected' : '' }} value="{{ $key }}">{{ $value }}</option>
-        @endforeach
-    </select>
+
+            @foreach($field->getOptions() as $key => $value)
+                <option 
+                    {{ in_array($key, (array) old($key, $field->getSelected() ?? $field->getValue($locale ?? null))) ? 'selected' : '' }} 
+                    value="{{ $key }}"
+                >{{ $value }}</option>
+            @endforeach
+        </select>
+
+        <span class="absolute pr-3 pointer-events-none">
+            <svg width="16" height="16" class="text-grey-700"><use xlink:href="#icon-chevron-down"></use></svg>
+        </span>
+    </div>
 @else
     <chief-multiselect
         name="{{ isset($locale) ? $field->getName($locale) : $field->getName() }}"
