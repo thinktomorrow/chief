5876156977e7784d8241029c9282894ce288442c
TijsTT <tijs@thinktomorrow.be>
2021-10-05 15:11:05 +0200

    Change: SortableToggle callbacks + style properties instead of classes

--- resources/assets/js/utilities/sortable-toggle.js
+++ resources/assets/js/utilities/sortable-toggle.js
@@ -1,8 +1,10 @@
 const SortableToggle = function (Sortables, options = {}) {
     this.Sortables = Sortables;
 
-    // Toggle
     this.isSorting = options.isSorting || false;
+    this.onShowSorting = options.onShowSorting || function () {};
+    this.onHideSorting = options.onHideSorting || function () {};
+
     this.sortToggles = Array.from(document.querySelectorAll('[data-sortable-toggle]'));
     this.hiddenWhenSortingEls = Array.from(document.querySelectorAll('[data-sortable-hide-when-sorting]'));
     this.showWhenSortingEls = Array.from(document.querySelectorAll('[data-sortable-show-when-sorting]'));
@@ -26,37 +28,41 @@ SortableToggle.prototype.toggle = function (e) {
         e.target.innerText = 'Stop met sorteren';
         this.showSorting();
     } else {
-        e.target.innerText = 'Sorteer handmatig';
+        e.target.innerText = 'Pas volgorde aan';
         this.hideSorting();
     }
 };
 
 SortableToggle.prototype.showSorting = function () {
     this.hiddenWhenSortingEls.forEach((el) => {
-        el.classList.add('hidden');
+        el.style.display = 'none';
     });
 
     this.showWhenSortingEls.forEach((el) => {
-        el.classList.remove('hidden');
+        el.style.removeProperty('display');
     });
 
     this.Sortables.forEach((sortableInstance) => {
         sortableInstance.option('disabled', false);
     });
+
+    this.onShowSorting();
 };
 
 SortableToggle.prototype.hideSorting = function () {
     this.hiddenWhenSortingEls.forEach((el) => {
-        el.classList.remove('hidden');
+        el.style.removeProperty('display');
     });
 
     this.showWhenSortingEls.forEach((el) => {
-        el.classList.add('hidden');
+        el.style.display = 'none';
     });
 
     this.Sortables.forEach((sortableInstance) => {
         sortableInstance.option('disabled', true);
     });
+
+    this.onHideSorting();
 };
 
 export { SortableToggle as default };
