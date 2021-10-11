1b38bff0e4680cd473daaf1517cdbb186ff28386
Ben Cavens <cavensben@gmail.com>
2021-10-11 17:32:28 +0200

    WIP

--- resources/views/manager/show.blade.php
+++ resources/views/manager/show.blade.php
@@ -1,14 +1,27 @@
 <x-chief::page>
 
-    MAIN
-    <x-chief::field.show tagged="top" />
+    <x-chief::field.show key="title" />
 
-    <x-chief::field.window title="Algemeen" tagged="top">
+    <x-chief::field key="title" />
+
+    <div class="window window-md window-white">
+        <x-chief::field.set type="error">
+            <x-chief::field.show key="title" />
+            <x-chief::field.show key="content" />
+        </x-chief::field.set>
+    </div>
+
+    <x-chief::field.window title="Categorieën" tagged="top">
         <x-chief::field.multiple tagged="top" />
+        <x-chief-inline-notification type="error">
+            OPGELET!!!!
+        </x-chief-inline-notification>
     </x-chief::field.window>
 
+    @include('back.shop.catalog.products.window.window');
+
     <x-slot name="sidebar">
-        <x-chief::field.multiple tagged="top" />
+        <x-chief::field.multiple tagged="sidebar" />
     </x-slot>
 
 </x-chief::page>
