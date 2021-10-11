2043db018ef668f1ff5657d2ac71601836d34b72
Ben Cavens <cavensben@gmail.com>
2021-10-11 08:22:12 +0200

    WIP fields as componetns

--- resources/views/components/field/show.blade.php
+++ resources/views/components/field/show.blade.php
@@ -1,1 +1,8 @@
-{!! $model->field($key)->renderRead() !!}
+@if(isset($tagged))
+    @foreach(\Thinktomorrow\Chief\ManagedModels\Fields\Fields::make($model->fields())->tagged($tagged)->allFields() as $field)
+        <x-chief::field.show :key="$field->getKey()" />
+    @endforeach
+@else
+    {!! $model->field($key)->renderRead() !!}
+@endif
+
---
+++ resources/views/manager/show.blade.php
@@ -0,0 +1,14 @@
+<x-chief::page>
+
+    MAIN
+    <x-chief::field.show tagged="top" />
+
+    <x-chief::field.window title="Algemeen" tagged="top">
+        <x-chief::field.multiple tagged="top" />
+    </x-chief::field.window>
+
+    <x-slot name="sidebar">
+        <x-chief::field.multiple tagged="top" />
+    </x-slot>
+
+</x-chief::page>
