eded1cec69176d41e656e3505d1d2104b95f6327
TijsTT <tijs@thinktomorrow.be>
2021-10-18 00:00:53 +0200

    Change: collapsible-navigation script + view changes (WIP)

---
+++ resources/assets/js/utilities/collapsible-navigation.js
@@ -0,0 +1,29 @@
+// Make navigation collapsible
+// Toggle to switch between collapsed and open state (default is collapsed, so maybe rename to expandable instead?)
+// --> normal state and collapsed state
+// Icons are always visible, other elements are hidden and showing them expands the navigation
+// The navigation in expanded state has a fixed width
+// If collapsed, clicking a navigation item with children will open a dropdown to the right
+// If expanded, clicking a navigation item will show the underlying items under it (without dropdown)
+// On mobile, the navigation is always expanded (full width) and works like a normal mobile navigation with hamburger icon
+const initCollapsibleNavigation = (
+    containerSelector = '[data-collapsible-navigation]',
+    toggleSelector = '[data-toggle-collapsible-navigation]',
+    collapsingElementSelector = '[data-hide-on-collapse]'
+) => {
+    const container = document.querySelector(containerSelector);
+    const toggle = document.querySelector(toggleSelector);
+    const collapsingElements = Array.from(document.querySelectorAll(collapsingElementSelector));
+
+    toggle.addEventListener('click', () => {
+        container.classList.toggle('w-64');
+
+        collapsingElements.forEach((element) => {
+            element.classList.toggle('hidden');
+        });
+    });
+
+    console.log(container, toggle, collapsingElements);
+};
+
+export { initCollapsibleNavigation as default };
--- resources/views/layout/nav/nav.blade.php
+++ resources/views/layout/nav/nav.blade.php
@@ -1,4 +1,26 @@
-<div class="flex flex-col h-screen bg-white shadow-sm window-md">
+<div data-collapsible-navigation class="w-64 h-screen p-4 bg-white">
+    <div data-toggle-collapsible-navigation class="px-4 py-2 rounded-lg cursor-pointer hover:bg-grey-100">
+        <div class="flex items-center justify-start space-x-4">
+            <div class="flex-shrink-0">
+                <svg width="20" height="20"><use xlink:href="#icon-home"></use></svg>
+            </div>
+
+            <span data-hide-on-collapse class="link link-black pop"> Dashboard </span>
+        </div>
+    </div>
+
+    <div class="px-4 py-2 rounded-lg cursor-pointer hover:bg-grey-100">
+        <div class="flex items-center justify-start space-x-4">
+            <div class="flex-shrink-0">
+                <svg width="20" height="20"><use xlink:href="#icon-collection"></use></svg>
+            </div>
+
+            <span data-hide-on-collapse class="link link-black pop"> Pagina's </span>
+        </div>
+    </div>
+</div>
+
+{{-- <div class="flex flex-col h-screen bg-white shadow-sm window-md">
     <div class="py-6" style="padding-right: calc(20px + 1rem);">
         <a href="{{ route('chief.back.dashboard') }}" class="link link-black">
             <x-chief-icon-label icon="icon-home" space="large">Dashboard</x-chief-icon-label>
@@ -22,4 +44,4 @@
             @include('chief::layout.nav.nav-user')
         </nav>
     </div>
-</div>
+</div> --}}
