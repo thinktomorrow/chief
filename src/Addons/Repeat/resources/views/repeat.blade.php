51466f34150df7b882028b198bce310ab3b7e8bb
TijsTT <tijs@thinktomorrow.be>
2021-11-10 10:20:14 +0100

    Fix: repeatfield styling

--- src/Addons/Repeat/resources/views/repeat.blade.php
+++ src/Addons/Repeat/resources/views/repeat.blade.php
@@ -9,14 +9,14 @@
 <div data-repeat-container="{{ $uniqueContainerId }}" class="relative">
     <div class="p-4">
         <div
-            class="{{ $repeatField->prefersCompactLayout() ? '' : 'border divide-y divide-grey-100 border-grey-100' }} rounded-lg -m-4"
+            class="{{ $repeatField->prefersCompactLayout() ? 'divide-y divide-grey-100' : 'border divide-y divide-grey-100 border-grey-100' }} rounded-lg -m-4"
             data-repeat-fields="{{ $uniqueContainerId }}"
         >
             @foreach($repeatedFields as $i => $fields)
                 <fieldset
                     id="{{ \Illuminate\Support\Str::random(8) }}"
                     data-repeat-fieldset="{{ $uniqueContainerId }}"
-                    class="flex {{ $repeatField->prefersCompactLayout() ? 'mb-4' : 'p-4' }} {{ $fields->count() == 1 ? 'items-center' : '' }}"
+                    class="flex items-center {{ $repeatField->prefersCompactLayout() ? 'py-2' : 'p-4' }}"
                 >
                     <div class="w-full">
                         <div class="row-start-start gutter-3">
@@ -51,7 +51,7 @@
 
     <span
         data-repeat-add="{{ $uniqueContainerId }}"
-        class="cursor-pointer link link-primary {{ $repeatField->prefersCompactLayout() ? 'mt-2' : 'mt-6' }}"
+        class="cursor-pointer link link-primary {{ $repeatField->prefersCompactLayout() ? 'mt-4' : 'mt-6' }}"
     >
         <x-chief-icon-label type="add">Nieuw veld toevoegen</x-chief-icon-label>
     </span>
