c6c11ec81fa9828b62bd5654024fa820692db255
BenCavens <BenCavens@users.noreply.github.com>
2022-03-14 20:38:30 +0000

    Fix styling

--- src/Forms/Fields/Number.php
+++ src/Forms/Fields/Number.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Thinktomorrow\Chief\Forms\Fields;
 
-use Thinktomorrow\Chief\Forms\Fields\Concerns\HasMinMax;
 use Thinktomorrow\Chief\Forms\Fields\Concerns\HasCharacterCount;
+use Thinktomorrow\Chief\Forms\Fields\Concerns\HasMinMax;
 use Thinktomorrow\Chief\Forms\Fields\Concerns\HasPrependAppend;
 
 class Number extends Component implements Field
