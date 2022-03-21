48d5e0ca48f41f1aad9f87f440cd92ba22cbba26
Tijs <tijs@thinktomorrow.be>
2022-03-21 09:40:47 +0100

    Change: package update + default window layout

--- src/Forms/resources/layouts/default-window.blade.php
+++ src/Forms/resources/layouts/default-window.blade.php
@@ -1,9 +1,5 @@
-<div class="-mx-3 p-3 rounded-lg bg-white">
-    <div class="space-y-6">
-        @foreach($getComponents() as $childComponent)
-            {{ $childComponent->editInSidebar() }}
-        @endforeach
-    </div>
+<div class="space-y-6">
+    @foreach($getComponents() as $childComponent)
+        {{ $childComponent->editInSidebar() }}
+    @endforeach
 </div>
-
-
