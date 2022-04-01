657ae97e98cb4932936ca6440340ce89e4d3fa30
Ben Cavens <cavensben@gmail.com>
2022-04-01 13:53:43 +0200

    Add: filter presets

---
+++ src/ManagedModels/Filters/FilterPresets.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Thinktomorrow\Chief\ManagedModels\Filters;
+
+use Thinktomorrow\Chief\ManagedModels\States\PageState;
+use Thinktomorrow\Chief\ManagedModels\Filters\Presets\RadioFilter;
+use Thinktomorrow\Chief\ManagedModels\Filters\Presets\InputFilter;
+
+class FilterPresets
+{
+    public static function state(): Filter
+    {
+        return RadioFilter::make('online', function($query, $value) {
+            return $query->where('current_state', '=', $value);
+        })->options([
+            '' => 'Alle',
+            PageState::PUBLISHED => 'online',
+            PageState::DRAFT => 'offline',
+        ]);
+    }
+
+    public static function text(array $dynamicColumns = ['title'], array $astrotomicColumns = [], string $label = 'titel'): Filter
+    {
+        return InputFilter::make('search', function($query, $value) use($dynamicColumns, $astrotomicColumns) {
+            return $query->where(function($builder) use($value, $dynamicColumns, $astrotomicColumns){
+                foreach($dynamicColumns as $column) {
+                    $builder->orWhereRaw('LOWER(json_extract(`values`, "$.'.$column.'.nl")) LIKE ?', '%'. trim(strtolower($value)) . '%');
+                }
+
+                foreach($astrotomicColumns as $column) {
+                    $builder->orWhereTranslationLike($column, '%'.$value.'%');
+                }
+
+                return $builder;
+            });
+        })->label($label);
+    }
+}
