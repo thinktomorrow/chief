995adcff3be12593dc512952a88c4c3df3c0be8f
Tijs <tijs@thinktomorrow.be>
2022-04-06 14:34:14 +0200

    Change: fragment-select-new styling

--- resources/views/manager/windows/fragments/component/fragment-select-new.blade.php
+++ resources/views/manager/windows/fragments/component/fragment-select-new.blade.php
@@ -1,40 +1,48 @@
-<div data-form data-form-tags="fragments" class="space-y-6">
+<div data-form data-form-tags="fragments" class="pt-6 space-y-6 border-t border-grey-100">
     <p class="text-lg display-base display-dark">Voeg een nieuw fragment toe</p>
 
-    <div class="row-start-stretch gutter-1.5">
+    <div>
         @forelse($fragments as $category => $fragmentsByCategory)
-            @if($category)
-                <h2 class="w-full mt-8 text-lg font-bold uppercase">{{ $category }}</h2>
-            @endif
+            <div @class(['space-y-3', 'pt-6 border-t border-grey-100' => !$loop->first, 'pb-6' => !$loop->last])>
+                @if($category)
+                    <div>
+                        <p class="text-sm tracking-wider uppercase display-base body-dark">{{ ucfirst($category) }}</p>
+                    </div>
+                @endif
 
-            @foreach($fragmentsByCategory as $allowedFragment)
-                <div class="w-full sm:w-1/2">
-                    <a
-                        data-sidebar-trigger
-                        href="{{ $allowedFragment['manager']->route('fragment-create', $owner) . (isset($order) ? '?order=' . $order : '') }}"
-                        title="{{ ucfirst($allowedFragment['resource']->getLabel()) }}"
-                        class="block p-3 space-y-3 transition-all duration-75 ease-in-out border rounded-lg border-grey-100 hover:shadow-card hover:border-primary-500"
-                    >
-                        <div class="flex items-center gap-3">
-                            <div class="flex items-center justify-center w-10 h-10 rounded-lg shrink-0 bg-grey-50 text-grey-500 children:w-6 children:h-6">
-                                {!! $allowedFragment['resource']->getIcon() !!}
-                            </div>
+                <div class="row-start-stretch gutter-1.5">
+                    @foreach($fragmentsByCategory as $allowedFragment)
+                        <div class="w-full sm:w-1/2">
+                            <a
+                                data-sidebar-trigger
+                                href="{{ $allowedFragment['manager']->route('fragment-create', $owner) . (isset($order) ? '?order=' . $order : '') }}"
+                                title="{{ ucfirst($allowedFragment['resource']->getLabel()) }}"
+                                class="flex gap-3 p-3 transition-all duration-75 ease-in-out border rounded-lg border-grey-100 bg-grey-50 hover:shadow-card hover:border-primary-500"
+                            >
+                                <div class="shrink-0 body-dark children:w-6 children:h-6">
+                                    {!! $allowedFragment['resource']->getIcon() !!}
+                                </div>
 
-                            <p class="leading-tight display-base display-dark">
-                                {{ ucfirst($allowedFragment['resource']->getLabel()) }}
-                            </p>
+                                <div class="space-y-2">
+                                    <p class="mt-[0.15rem] leading-tight display-base display-dark">
+                                        {{ ucfirst($allowedFragment['resource']->getLabel()) }}
+                                    </p>
+
+                                    @if($hint = $allowedFragment['resource']->getHint())
+                                        <p class="text-sm body-base body-dark">
+                                            {!! $hint !!}
+                                        </p>
+                                    @endif
+                                </div>
+                            </a>
                         </div>
-
-                        @if($hint = $allowedFragment['resource']->getHint())
-                            <p class="text-sm body-base body-dark">
-                                {!! $hint !!}
-                            </p>
-                        @endif
-                    </a>
+                    @endforeach
                 </div>
-            @endforeach
+            </div>
         @empty
-            <p class="body-base body-dark">Geen fragmenten gevonden.</p>
+            <div class="w-full">
+                <p class="body-base body-dark">Geen fragmenten gevonden.</p>
+            </div>
         @endforelse
     </div>
 </div>
