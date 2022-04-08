9cb4765486d02fdfeac58a668362aa715fc32e4e
Ben Cavens <cavensben@gmail.com>
2022-04-08 11:18:54 +0200

    Fix: missing space

--- src/Fragments/FragmentStatus.php
+++ src/Fragments/FragmentStatus.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Thinktomorrow\Chief\Fragments;
 
-enum FragmentStatus:string
+enum FragmentStatus: string
 {
     case online = 'online';
     case offline = 'offline';
