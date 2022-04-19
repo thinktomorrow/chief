e65d148671db101b515c64aa052582bafd8a53e2
Tijs <tijs@thinktomorrow.be>
2022-04-19 16:10:35 +0200

    Fix: image field and card layout style fixing

--- src/Forms/resources/layouts/card.blade.php
+++ src/Forms/resources/layouts/card.blade.php
@@ -5,8 +5,10 @@
     <div class="space-y-6">
         @include('chief-form::layouts._partials.header')
 
-        @foreach($getComponents() as $childComponent)
-            {{ $childComponent }}
-        @endforeach
+        <div>
+            @foreach($getComponents() as $childComponent)
+                {{ $childComponent }}
+            @endforeach
+        </div>
     </div>
 </div>
