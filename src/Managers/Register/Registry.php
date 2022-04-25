d569db3ca3a6df6e5a54dc2a5d782e7f621fcfdf
BenCavens <BenCavens@users.noreply.github.com>
2022-04-25 11:56:29 +0000

    Fix styling

--- src/Managers/Register/Registry.php
+++ src/Managers/Register/Registry.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 namespace Thinktomorrow\Chief\Managers\Register;
 
-use Thinktomorrow\Chief\Managers\Exceptions\ResourceAlreadyRegistered;
 use Thinktomorrow\Chief\Managers\Exceptions\MissingResourceRegistration;
+use Thinktomorrow\Chief\Managers\Exceptions\ResourceAlreadyRegistered;
 use Thinktomorrow\Chief\Managers\Manager;
 use Thinktomorrow\Chief\Resource\PageResource;
 use Thinktomorrow\Chief\Resource\Resource;
@@ -93,7 +93,7 @@ final class Registry
 
     public function registerResource(string $key, Resource $resource, Manager $manager): self
     {
-        if(isset($this->resources[$key])) {
+        if (isset($this->resources[$key])) {
             throw new ResourceAlreadyRegistered('Cannot register resource. The resource key [' . $key . '] is already registered.');
         }
 
