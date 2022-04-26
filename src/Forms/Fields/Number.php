a1ea230c6cb367ff83ece3d938d590e4a4cef331
Ben Cavens <cavensben@gmail.com>
2022-04-26 09:52:12 +0200

    Add: number step attribute

--- src/Forms/Fields/Number.php
+++ src/Forms/Fields/Number.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Thinktomorrow\Chief\Forms\Fields;
 
+use Thinktomorrow\Chief\Forms\Fields\Concerns\HasStep;
 use Thinktomorrow\Chief\Forms\Fields\Concerns\HasCharacterCount;
 use Thinktomorrow\Chief\Forms\Fields\Concerns\HasMinMax;
 use Thinktomorrow\Chief\Forms\Fields\Concerns\HasPrependAppend;
@@ -13,6 +14,7 @@ class Number extends Component implements Field
     use HasCharacterCount;
     use HasPrependAppend;
     use HasMinMax;
+    use HasStep;
 
     protected string $view = 'chief-form::fields.number';
     protected string $windowView = 'chief-form::fields.text-window';
--- src/Forms/resources/fields/number.blade.php
+++ src/Forms/resources/fields/number.blade.php
@@ -3,5 +3,6 @@
         'type' => 'number',
         'min' => $getMin() ?? null,
         'max' => $getMax() ?? null,
+        'step' => $getStep() ?? null,
     ],
 ])
