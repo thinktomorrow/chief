79b8f4a41e62b60551e4a54b5fe525b06a83e3b9
BenCavens <BenCavens@users.noreply.github.com>
2022-08-01 06:21:15 +0000

    Fix styling

--- app/Listeners/LogSuccessfulLogin.php
+++ app/Listeners/LogSuccessfulLogin.php
@@ -8,7 +8,9 @@ class LogSuccessfulLogin
 {
     public function handle(Login $event)
     {
-        if($event->guard !== 'chief') return;
+        if ($event->guard !== 'chief') {
+            return;
+        }
 
         $event->user->last_login = date('Y-m-d H:i:s');
         $event->user->save();
