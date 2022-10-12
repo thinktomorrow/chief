b8ca4cc829f2b754ba12bf363232289f26842bb7
Ben Cavens <cavensben@gmail.com>
2022-10-12 16:36:07 +0200

    Fix

--- src/Table/Elements/TableCell.php
+++ src/Table/Elements/TableCell.php
@@ -6,11 +6,4 @@ namespace Thinktomorrow\Chief\Table\Elements;
 class TableCell extends Component
 {
     protected string $view = 'chief-table::cells.text';
-
-    // Text
-    // link
-    // label
-    // icon (edit button)
-    // image
-    // raw
 }
