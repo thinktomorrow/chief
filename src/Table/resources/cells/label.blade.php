7133ccaa67b40abc74dc8e42f6285c369124d417
Ben Cavens <cavensben@gmail.com>
2022-10-12 15:23:57 +0200

    Fix: layouttype

--- src/Table/resources/cells/label.blade.php
+++ src/Table/resources/cells/label.blade.php
@@ -1,7 +1,7 @@
 <x-chief::table.data>
     <span
         {{ $attributes->merge($getCustomAttributes())->class([
-            'label', 'label-xs', 'label-'.$getLayoutType(),
+            'label', 'label-xs', 'label-'.$getLayoutType()->value,
         ]) }}
         title="{{ $getDescription() }}"
     >
