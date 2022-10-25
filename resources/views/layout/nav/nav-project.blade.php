8da51fc39ed8ccdb5b7060438ef9f4609555ff26
Tijs <tijs@thinktomorrow.be>
2022-10-25 16:15:19 +0200

    Fix: nav-project border + CreateAdmin command capitalised labels

--- resources/views/layout/nav/nav-project.blade.php
+++ resources/views/layout/nav/nav-project.blade.php
@@ -1,7 +1,5 @@
-<hr class="my-6 border-grey-100">
-
-<x-chief::nav title="Pagina's" tagged="nav-page"></x-chief::nav>
-<x-chief::nav title="Bedrijf" tagged="nav-company"></x-chief::nav>
-<x-chief::nav tagged="nav" inline></x-chief::nav>
+<x-chief::nav title="Pagina's" tagged="nav-page"/>
+<x-chief::nav title="Bedrijf" tagged="nav-company"/>
+<x-chief::nav tagged="nav" inline/>
 
 <hr class="my-6 border-grey-100">
