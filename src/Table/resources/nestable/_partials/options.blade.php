8032e2273b263251c136641c986e43ff77cb8a11
Tijs <tijs@thinktomorrow.be>
2022-12-14 12:05:59 +0100

    Fix: nestable node options

--- src/Table/resources/nestable/_partials/options.blade.php
+++ src/Table/resources/nestable/_partials/options.blade.php
@@ -1,28 +1,28 @@
 <options-dropdown class="link link-primary">
     <div v-cloak class="dropdown-content">
-        @adminCan('edit', $model)
+        @adminCan('edit', $node->getModel())
             <a href="@adminRoute('edit', $node->getId())" title="Aanpassen" class="dropdown-link">
                 Aanpassen
             </a>
         @endAdminCan
 
-        @adminCan('preview', $model)
+        @adminCan('preview', $node->getModel())
             <a href="{{ $node->url() }}" title="Bekijk op site" target="_blank" class="dropdown-link">
                 Bekijk op site
             </a>
         @endAdminCan
 
-        @adminCan('duplicate', $model)
+        @adminCan('duplicate', $node->getModel())
             <button
                 type="submit"
-                form="duplicateForm-{{ $model->id }}"
-                class="cursor-pointer dropdown-link dropdown-link-success"
+                form="duplicateForm-{{ $node->getId() }}"
+                class="text-left cursor-pointer dropdown-link dropdown-link-success"
             >
                 Kopieer
             </button>
 
             <form
-                id="duplicateForm-{{ $model->id }}"
+                id="duplicateForm-{{ $node->getId() }}"
                 action="@adminRoute('duplicate', $node->getId())"
                 method="POST"
                 class="hidden"
