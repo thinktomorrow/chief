deeca677658aff5875d208f6058b1df82af23e24
Ben Cavens <cavensben@gmail.com>
2023-01-04 14:01:40 +0100

    Dont show menu title if nu menu items

--- resources/views/layout/nav/nav-project.blade.php
+++ resources/views/layout/nav/nav-project.blade.php
@@ -1,5 +1,6 @@
-<x-chief::nav title="Pagina's" tagged="nav-page" />
-<x-chief::nav title="Bedrijf" tagged="nav-company"/>
+<x-chief::nav title="Catalogus" inline tagged="nav-catalog"/>
+<x-chief::nav title="Blog" inline tagged="nav-blog"/>
+<x-chief::nav title="Bedrijf" inline tagged="nav-company"/>
 <x-chief::nav tagged="nav" inline/>
 
 <hr class="my-6 border-grey-100">
