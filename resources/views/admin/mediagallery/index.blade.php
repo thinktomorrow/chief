06d1fcbd1c821d888d17b48e711bf51960e7ac6c
Tijs <tijs@thinktomorrow.be>
2023-01-23 13:51:31 +0100

    Change: mediagallery index

--- resources/views/admin/mediagallery/index.blade.php
+++ resources/views/admin/mediagallery/index.blade.php
@@ -19,79 +19,84 @@
 @section('content')
     <div class="container">
         <div class="row gutter-3">
-            <div class="w-full lg:w-2/3">
-                <div class="card">
-                    <form method="POST" action="{{ route('chief.mediagallery.bulk') }}" id="selecting">
-                        <div class="flex items-center justify-between mb-4">
+            <div class="w-full">
+                <div class="space-y-4 card">
+                    <form data-form-submit-on-change method="GET" id="filtering" class="row-start-end gutter-3">
+                        <div class="w-full sm:w-1/2 md:w-1/3">
+                            <x-chief-form::formgroup id="name" label="Bestandsnaam">
+                                <input
+                                    type="text"
+                                    name="search"
+                                    placeholder="Zoek op bestandsnaam ..."
+                                    value="{{ old('search', request()->input('search'))}}"
+                                >
+                            </x-chief-form::formgroup>
+                        </div>
+
+                        <div class="w-full sm:w-1/2 md:w-1/3">
+                            <x-chief-form::formgroup id="owner" label="Pagina">
+                                <chief-multiselect
+                                    name="owner"
+                                    :options='@json($pages)'
+                                    selected='@json(old('owner', request()->input('owner')))'
+                                    :multiple='false'
+                                    grouplabel="group"
+                                    groupvalues="values"
+                                    labelkey="label"
+                                    valuekey="id"
+                                />
+                            </x-chief-form::formgroup>
+                        </div>
+
+                        <div class="w-full sm:w-1/2 md:w-1/3">
+                            <x-chief-form::formgroup id="unused">
+                                <label for="unused" class="with-checkbox">
+                                    <input
+                                        type="checkbox"
+                                        id="unused"
+                                        name="unused"
+                                        {{ old('unused', request()->input('unused')) ? 'checked' : '' }}
+                                    />
+
+                                    <span>Toon enkel ongebruikte media</span>
+                                </label>
+                            </x-chief-form::formgroup>
+                        </div>
+
+                        {{-- <button type="submit" form="filtering" class="btn btn-primary">Filter</button> --}}
+                    </form>
+
+                    <form method="POST" action="{{ route('chief.mediagallery.bulk') }}" id="selecting" class="space-y-6">
+                        <div class="flex items-center justify-between">
                             <label for="select-all" class="with-checkbox">
                                 <input type="checkbox" name="select_all" id="select-all">
                                 <span>Alles selecteren</span>
                             </label>
 
                             <div class="space-x-2">
+                                {{-- TODO: this shouldn't download a file if no images are selected (now it downloads empty .zip file) --}}
                                 <button type="submit" form="selecting" name="type" value="download" class="btn btn-primary">Download de selectie</button>
                                 <button v-cloak @click="showModal('mediagallery-bulk-delete-modal')" type="button" class="btn btn-error-outline">Verwijder de selectie</button>
                             </div>
                         </div>
 
-                        <div class="row gutter-3">
-                            @foreach($assets as $index => $asset)
-                                <div class="w-1/2 xl:w-1/3 2xl:w-1/4">
-                                    @include('chief::admin.mediagallery.item')
-                                </div>
-                            @endforeach
+                        <div>
+                            <div class="row gutter-3">
+                                @foreach($assets as $index => $asset)
+                                    <div class="w-1/2 lg:w-1/3 xl:w-1/4 2xl:w-1/5">
+                                        @include('chief::admin.mediagallery.item')
+                                    </div>
+                                @endforeach
+                            </div>
                         </div>
-                    </form>
 
-                    <div class="mt-8">
-                        {{ $assets->links('chief::pagination.default') }}
-                    </div>
+                        @if ($assets->total() > $assets->count())
+                            <div>
+                                {{ $assets->links('chief::pagination.default') }}
+                            </div>
+                        @endif
+                    </form>
                 </div>
-            </div>
-
-            <div class="w-full lg:w-1/3">
-                <x-chief-window title="Filter" class="card">
-                    <form method="GET" id="filtering" class="space-y-4">
-                        <span class="text-grey-500">{{ $assets->total() }} resultaten</span>
-
-                        <x-chief-form::formgroup id="name" label="Bestandsnaam">
-                            <input
-                                type="text"
-                                name="search"
-                                placeholder="Zoek op bestandsnaam ..."
-                                value="{{ old('search', request()->input('search'))}}"
-                            >
-                        </x-chief-form::formgroup>
-
-                        <x-chief-form::formgroup id="owner" label="Pagina">
-                            <chief-multiselect
-                                name="owner"
-                                :options='@json($pages)'
-                                selected='@json(old('owner', request()->input('owner')))'
-                                :multiple='false'
-                                grouplabel="group"
-                                groupvalues="values"
-                                labelkey="label"
-                                valuekey="id"
-                            ></chief-multiselect>
-                        </x-chief-form::formgroup>
-
-                        <x-chief-form::formgroup id="unused">
-                            <label for="unused" class="with-checkbox">
-                                <input
-                                    type="checkbox"
-                                    name="unused"
-                                    id="unused"
-                                    {{ old('unused', request()->input('unused')) ? 'checked' : '' }}
-                                >
-
-                                <span>Toon enkel ongebruikte media</span>
-                            </label>
-                        </x-chief-form::formgroup>
-
-                        <button type="submit" form="filtering" class="btn btn-primary">Filter</button>
-                    </form>
-                </x-chief-window>
             </div>
         </div>
     </div>
