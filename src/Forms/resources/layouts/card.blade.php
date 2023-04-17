1b20c12b9324acf692d9305406c2f8724ae87263
Tijs <tijs@thinktomorrow.be>
2023-04-17 13:26:39 +0200

    Fix: Card layout renders fields without spacing

--- src/Forms/resources/layouts/card.blade.php
+++ src/Forms/resources/layouts/card.blade.php
@@ -5,7 +5,7 @@
     <div class="space-y-6">
         @include('chief-form::layouts._partials.header')
 
-        <div>
+        <div class="space-y-6">
             @foreach($getComponents() as $childComponent)
                 {{ $childComponent }}
             @endforeach
