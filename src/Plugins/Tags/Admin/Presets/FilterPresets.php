71b3c96f5f535f075057d9cb5c42a8c02ea19791
Ben Cavens <cavensben@gmail.com>
2023-04-21 11:46:22 +0200

    Filter tags

--- src/Plugins/Tags/Admin/Presets/FilterPresets.php
+++ src/Plugins/Tags/Admin/Presets/FilterPresets.php
@@ -11,15 +11,31 @@ class FilterPresets
 {
     public static function tags(): Filter
     {
-        return SelectFilter::make('tags', function ($query, $value) {
-            $tagIds = (array) $value;
+        return (new TagsFilter);
 
-            $query->whereHas('tags', function (Builder $q) use ($tagIds) {
-                $q->whereIn('id', $tagIds);
-            });
-        })->label('Tag')
-        ->options(app(TagReadRepository::class)->getAllForSelect())
-        ->default('')
-        ->view('chief-tags::filters.tags');
+//        return SelectFilter::make('tags', function ($query, $value) {
+//            $tagIds = (array) $value;
+//
+//            $query->whereHas('tags', function (Builder $q) use ($tagIds) {
+//                $q->whereIn('id', $tagIds);
+//            });
+//        })->label('Tag')
+//        ->options(app(TagReadRepository::class)->getAllForSelect())
+//        ->default('')
+//        ->view('chief-tags::filters.tags');
     }
+
+//    public static function tags(): Filter
+//    {
+//        return SelectFilter::make('tags', function ($query, $value) {
+//            $tagIds = (array) $value;
+//
+//            $query->whereHas('tags', function (Builder $q) use ($tagIds) {
+//                $q->whereIn('id', $tagIds);
+//            });
+//        })->label('Tag')
+//            ->options(app(TagReadRepository::class)->getAllForSelect())
+//            ->default('')
+//            ->view('chief-tags::filters.tags');
+//    }
 }
--- src/Plugins/Tags/Admin/resources/views/filters/tags.blade.php
+++ src/Plugins/Tags/Admin/resources/views/filters/tags.blade.php
@@ -13,37 +13,37 @@
     @endif
 
     <div class="flex flex-wrap items-start gap-1">
-        @foreach ($options as $color => $label)
-            <x-chief::input.label for="{{ $id . '-' . $color }}" class="cursor-pointer">
+        @foreach ($tags as $tag)
+            <x-chief::input.label for="{{ $tag->getTagId() . '-' . $tag->getColor() }}" class="cursor-pointer">
                 <x-chief::input.radio
-                    id="{{ $id . '-' . $color }}"
+                    id="{{ $tag->getTagId() . '-' . $tag->getColor() }}"
                     name="{{ $name }}"
-                    value="{{ $label }}"
+                    value="{{ $tag->getTagId() }}"
                     class="hidden peer"
                 />
 
                 <x-chief-tags::tag
-                    :color="$color"
+                    :color="$tag->getColor()"
                     size="sm"
                     class="peer-checked:ring-1 peer-checked:ring-primary-500"
                 >
-                    {{ $label }}
+                    {{ $tag->getLabel() }}
                 </x-chief-tags::tag>
             </x-chief::input.label>
         @endforeach
     </div>
 
-    <chief-multiselect
-        id="{{ $id }}"
-        name="{{ $name }}"
-        :options='@json($options)'
-        selected='@json($value ?: $default)'
-        :multiple='@json($multiple)'
-        @if($isGrouped)
-            grouplabel="group"
-            groupvalues="values"
-            labelkey="label"
-            valuekey="id"
-        @endif
-    />
+{{--    <chief-multiselect--}}
+{{--        id="{{ $id }}"--}}
+{{--        name="{{ $name }}"--}}
+{{--        :options='@json($options)'--}}
+{{--        selected='@json($value ?: $default)'--}}
+{{--        :multiple='@json($multiple)'--}}
+{{--        @if($isGrouped)--}}
+{{--            grouplabel="group"--}}
+{{--            groupvalues="values"--}}
+{{--            labelkey="label"--}}
+{{--            valuekey="id"--}}
+{{--        @endif--}}
+{{--    />--}}
 </x-chief::input.group>
