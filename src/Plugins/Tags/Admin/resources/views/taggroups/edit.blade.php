4faf8f53741f21cfaa1c5e2f757efbf1c10326d3
Ben Cavens <cavensben@gmail.com>
2023-04-25 11:42:11 +0200

    Fix: delete tag

--- src/Plugins/Tags/Admin/resources/views/taggroups/edit.blade.php
+++ src/Plugins/Tags/Admin/resources/views/taggroups/edit.blade.php
@@ -18,19 +18,57 @@
                 @foreach($fields as $field)
                     {!! $field->render() !!}
                 @endforeach
-            </div>
 
-            <div class="flex justify-between">
-                <button class="mt-4 btn btn-primary" type="submit">Bewaar aanpassingen</button>
-                <a href="{{ route('chief.taggroups.delete', $model) }}" title="Groep verwijderen" class="link link-error">
-                    <x-chief::icon-label icon="icon-trash">
-                        Groep verwijderen
-                    </x-chief::icon-label>
-                </a>
-            </div>
+                <div class="flex justify-between items-center mt-4">
+                    <button class="btn btn-primary" type="submit">Bewaar aanpassingen</button>
 
+                    <a
+                        v-cloak
+                        @click="showModal('delete-taggroup-modal-{{ $model->id }}')"
+                        class="block cursor-pointer"
+                    >
+                        <x-chief::icon-label icon="icon-trash" color="grey" class="bg-white shadow-none text-grey-500">
+                            Verwijder groep
+                        </x-chief::icon-label>
+                    </a>
+                </div>
+            </div>
 
         </form>
+
+
+        @push('portals')
+            <modal
+                id="delete-taggroup-modal-{{ $model->id }}"
+                title="Wil je deze groep verwijderen?"
+            >
+                <p>
+
+                </p>
+
+                <form
+                    id="delete-taggroup-modal-form-{{ $model->id }}"
+                    action="{{ route('chief.taggroups.delete', $model->id) }}"
+                    method="POST"
+                    v-cloak
+                >
+                    @csrf
+                    @method('DELETE')
+                </form>
+
+                <div v-cloak slot="modal-action-buttons">
+                    <button
+                        form="delete-taggroup-modal-form-{{ $model->id }}"
+                        type="submit"
+                        class="btn btn-error"
+                    >
+                        <x-chief::icon-label icon="icon-trash">
+                            Ja, groep verwijderen
+                        </x-chief::icon-label>
+                    </button>
+                </div>
+            </modal>
+        @endpush
 
     </x-chief::page.grid>
 </x-chief::page.template>
--- src/Plugins/Tags/Admin/resources/views/tags/edit.blade.php
+++ src/Plugins/Tags/Admin/resources/views/tags/edit.blade.php
@@ -22,9 +22,55 @@
                 @foreach($fields as $field)
                     {!! $field->render() !!}
                 @endforeach
-            </div>
 
-            <button class="mt-4 btn btn-primary" type="submit">Bewaar aanpassingen</button>
+                <div class="flex justify-between items-center mt-4">
+                    <button class="btn btn-primary" type="submit">Bewaar aanpassingen</button>
+
+                    <a
+                        v-cloak
+                        @click="showModal('delete-tag-modal-{{ $model->id }}')"
+                        class="block cursor-pointer"
+                    >
+                        <x-chief::icon-label icon="icon-trash" color="grey" class="bg-white shadow-none text-grey-500">
+                            Verwijder tag
+                        </x-chief::icon-label>
+                    </a>
+                </div>
+            </div>
         </form>
     </x-chief::page.grid>
+
+    @push('portals')
+        <modal
+            id="delete-tag-modal-{{ $model->id }}"
+            title="Wil je deze tag verwijderen?"
+        >
+            <p>
+                Als je de tag verwijdert, zal deze ook worden ontkoppeld van alle pagina's.
+            </p>
+
+            <form
+                id="delete-tag-modal-form-{{ $model->id }}"
+                action="{{ route('chief.tags.delete', $model->id) }}"
+                method="POST"
+                v-cloak
+            >
+                @csrf
+                @method('DELETE')
+            </form>
+
+            <div v-cloak slot="modal-action-buttons">
+                <button
+                    form="delete-tag-modal-form-{{ $model->id }}"
+                    type="submit"
+                    class="btn btn-error"
+                >
+                    <x-chief::icon-label icon="icon-trash">
+                        Ja, tag verwijderen
+                    </x-chief::icon-label>
+                </button>
+            </div>
+        </modal>
+    @endpush
+
 </x-chief::page.template>
