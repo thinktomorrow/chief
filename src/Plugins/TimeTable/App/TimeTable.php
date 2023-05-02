2dd386ab9ffc20e97ac53995b7b6574ccbfe9740
Ben Cavens <cavensben@gmail.com>
2023-05-02 11:05:44 +0200

    Add days for timetable api

--- src/Plugins/TimeTable/App/TimeTable.php
+++ src/Plugins/TimeTable/App/TimeTable.php
@@ -2,29 +2,42 @@
 
 namespace Thinktomorrow\Chief\Plugins\TimeTable\App;
 
+use Carbon\Carbon;
 use Spatie\OpeningHours\OpeningHours;
 
 class TimeTable extends OpeningHours
 {
     public function forCurrentWeek(): array
     {
-        $now = now();
-        $monday = $now->copy()->startOfWeek();
-        $tuesday = $monday->copy()->addDay();
-        $wednesday = $monday->copy()->addDays(2);
-        $thursday = $monday->copy()->addDays(3);
-        $friday = $monday->copy()->addDays(4);
-        $saturday = $monday->copy()->addDays(5);
-        $sunday = $monday->copy()->addDays(6);
+        return $this->forWeeks(1);
+    }
 
-        return [
-            $this->forDate($monday),
-            $this->forDate($tuesday),
-            $this->forDate($wednesday),
-            $this->forDate($thursday),
-            $this->forDate($friday),
-            $this->forDate($saturday),
-            $this->forDate($sunday),
-        ];
+    public function forWeeks(int $weeks = 2, int $weekStartsAt = 1): array
+    {
+        $from = now()->startOfWeek($weekStartsAt);
+        $until = $from->copy()->addDays((7 * $weeks)-1);
+
+        return $this->forDays($from, $until);
+    }
+
+    public function forDays(Carbon $from, Carbon $until): array
+    {
+        $days = [];
+
+//        $i = 1;
+        $period = $from->range($until);
+
+        foreach($period as $day) {
+            $days[] = $this->forDate($day);
+        }
+
+//        $day = $from->copy();
+//        $days[] = $this->forDate($day);
+//
+//        while($until->gt($day)) {
+//            $days[] = $this->forDate($day->copy()->addDays(++$i));
+//        }
+
+        return $days;
     }
 }
