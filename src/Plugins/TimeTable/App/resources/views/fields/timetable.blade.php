fa965b21756d0072d2f66a5da072f785db4bb2c3
Ben Cavens <cavensben@gmail.com>
2023-05-08 16:40:14 +0200

    Couple of fixes

--- src/Plugins/TimeTable/App/resources/views/fields/timetable.blade.php
+++ src/Plugins/TimeTable/App/resources/views/fields/timetable.blade.php
@@ -1,11 +1,14 @@
 @php
     $timetable_id = $getActiveValue($locale ?? null);
 
-    $timeTable = (new \Thinktomorrow\Chief\Plugins\TimeTable\App\TimeTableFactory())->create(
+    $timeTable = $timetable_id ? (new \Thinktomorrow\Chief\Plugins\TimeTable\App\TimeTableFactory())->create(
         $timeTableModel = \Thinktomorrow\Chief\Plugins\TimeTable\Infrastructure\Models\TimeTableModel::find($timetable_id),
         app()->getLocale(),
-    );
+    ) : null;
 @endphp
 
+@if($timeTable)
 <x-chief-timetable::time-table :time-table="$timeTable" :days="$timeTable->forWeeks(13)" :read="true"/>
-{{--<x-chief-timetable::time-table :model="$timeTable" :days="$timeTable->getDays()" :read="true"/>--}}
+
+<a href="{{ route('chief.timetables.edit', $timetable_id) }}">bewerk deze openingsuren</a>
+@endif
