eea4e8597f3868bb304fa3bf3a9ee23d9d3d845a
Tijs <tijs@thinktomorrow.be>
2024-02-16 17:00:28 +0100

    Change: TableNew table container layout (WIP)

--- src/TableNew/UI/views/index.blade.php
+++ src/TableNew/UI/views/index.blade.php
@@ -24,53 +24,53 @@
 
 <x-chief::page.layout>
     <div class="flex justify-center p-16">
-        <div class="overflow-x-auto w-160 whitespace-nowrap">
+        <div class="overflow-x-auto bg-white rounded-lg shadow-md w-160 whitespace-nowrap ring-1 ring-grey-300">
             {{-- ACTIES
             BULKACTIES (context)
             ROWACTIES
             FILTER
             SORT --}}
 
-            <table class="min-w-full divide-y table-fixed divide-grey-400">
+            <table class="min-w-full divide-y table-fixed divide-grey-300">
                 <thead>
                     <tr>
-                        <th scope="col" class="w-6 py-2 pr-3 text-left">
+                        <th scope="col" class="w-6 py-3 pl-4 pr-3 text-left">
                             <div class="flex items-center">
                                 <input type="checkbox" name="all" id="all" class="w-4 h-4 decoration-primary-500"/>
                             </div>
                         </th>
 
-                        <th scope="col" class="py-2 pl-3 pr-3 text-left">Id</th>
-                        <th scope="col" class="py-2 pl-3 pr-3 text-left">Title</th>
+                        <th scope="col" class="py-3 pl-3 pr-3 font-medium text-left">Id</th>
+                        <th scope="col" class="py-3 pl-3 pr-3 font-medium text-left">Title</th>
 
-                        <th scope="col" class="py-2 pl-3 text-left">
+                        <th scope="col" class="py-3 pl-3 pr-4 font-medium text-left">
                             <span class="sr-only">Aanpassen</span>
                         </th>
                     </tr>
                 </thead>
 
-                <tbody class="divide-y divide-grey-300">
+                <tbody class="divide-y divide-grey-200">
                     @foreach($rows as $row)
                         <tr>
-                            <td class="py-1.5 pr-3 text-left w-6">
+                            <td class="w-6 py-2 pl-4 pr-3 text-left">
                                 <div class="flex items-center">
                                     <input type="checkbox" name="{{ $row->id }}" id="{{ $row->id }}" class="w-4 h-4 decoration-primary-500"/>
                                 </div>
                             </td>
 
-                            <td class="py-1.5 pl-3 pr-3 text-left">
+                            <td class="py-2 pl-3 pr-3 text-left">
                                 <span class="leading-5 text-grey-700">
                                     {{ $row->id }}
                                 </span>
                             </td>
 
-                            <td class="py-1.5 pl-3 pr-3 text-left">
+                            <td class="py-2 pl-3 pr-3 text-left">
                                 <span class="leading-5 text-grey-700">
                                     {{ $row->title }} {{ $row->id }}
                                 </span>
                             </td>
 
-                            <td class="py-1.5 pl-3 text-right">
+                            <td class="py-2 pl-3 pr-4 text-right">
                                 <button type="button" class="font-medium leading-5 text-primary-500 hover:text-primary-600">
                                     Aanpassen
                                 </button>
@@ -80,13 +80,13 @@
                         @if(isset($row->rows))
                             @foreach($row->rows as $childRow)
                                 <tr>
-                                    <td class="py-1.5 pr-3 text-left w-6">
+                                    <td class="w-6 py-2 pl-4 pr-3 text-left">
                                         <div class="flex items-center">
                                             <input type="checkbox" name="{{ $row->id }}" id="{{ $row->id }}" class="w-4 h-4 decoration-primary-500"/>
                                         </div>
                                     </td>
 
-                                    <td class="py-1.5 pl-3 pr-3 text-left">
+                                    <td class="py-2 pl-3 pr-3 text-left">
                                         <div class="flex items-start gap-2">
                                             <div style="width: {{ 20 * $level }}px;" class="flex items-end">
                                                 <svg class="w-5 h-5 text-grey-700" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 256 256"><path d="M229.66,157.66l-48,48a8,8,0,0,1-11.32-11.32L204.69,160H128A104.11,104.11,0,0,1,24,56a8,8,0,0,1,16,0,88.1,88.1,0,0,0,88,88h76.69l-34.35-34.34a8,8,0,0,1,11.32-11.32l48,48A8,8,0,0,1,229.66,157.66Z"></path></svg>
@@ -98,13 +98,13 @@
                                         </div>
                                     </td>
 
-                                    <td class="py-1.5 pl-3 pr-3 text-left">
+                                    <td class="py-2 pl-3 pr-3 text-left">
                                         <span class="leading-5 text-grey-700">
                                             {{ $childRow->title }} {{ $childRow->id }}
                                         </span>
                                     </td>
 
-                                    <td class="py-1.5 pl-3 text-right">
+                                    <td class="py-2 pl-3 pr-4 text-right">
                                         <button type="button" class="font-medium leading-5 text-primary-500 hover:text-primary-600">
                                             Aanpassen
                                         </button>
