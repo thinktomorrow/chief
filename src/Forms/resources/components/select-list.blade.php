00be966f0e60547b7c734d469cc2cf810623bbb9
Tijs <tijs@thinktomorrow.be>
2024-04-26 15:59:04 +0200

    Change: select-list layout (WIP)

--- src/Forms/resources/components/select-list.blade.php
+++ src/Forms/resources/components/select-list.blade.php
@@ -1,19 +1,19 @@
 @props([
-  'options' => [],
-  'selection' => [],
-  'multiple' => false,
-  'placeholder' => null,
-  'name' => null,
+    'options' => [],
+    'selection' => [],
+    'multiple' => false,
+    'placeholder' => null,
+    'name' => null,
 ])
 
-// values loop over and create list
+{{-- // values loop over and create list
 // each list item is sortable and deletable
 // adding item to list is done by selecting from the multiselect
 // After adding, the value is removed from select
 // And is not shown in the select dropdown list
 
 // TODO: check if wire:model is possible to use here
-// TODO: allow rich html for each option in the list
+// TODO: allow rich html for each option in the list --}}
 
 <div
     x-cloak
@@ -37,40 +37,35 @@
         sortSelection: (sortedSelection) => {
             $data.selection = sortedSelection;
             $data.onSelectionChange();
+        },
+        getOptionLabel: (option) => {
+            return $data.options.find(opt => opt.value === option)['label'];
         }
     }"
-    x-init="
-        $nextTick(() => {
-
-            const refreshOptions = () => {
-                // Filter out the selected options from the options list
-                $data.filteredOptions = $data.options.filter(option => !$data.selection.includes(option.value));
-
-                $el.choices.clearStore();
-                $el.choices.setChoices($data.filteredOptions);
-            }
-
-            $data.onSelectionChange = () => {
-
-                // Notify change event for outside listeners, such as the Conditional fields js.
-                $dispatch('select-list-change');
-
-                // Notify wired model
-                $dispatch('input', $data.selection);
-
-                refreshOptions();
-            }
-
-            $data.addItem = (e) => {
-
-                // Merge newValue with current selection
-                $data.selection = [...$data.selection, ...$el.choices.getValue(true)];
-                $data.onSelectionChange();
-            }
-
+    x-init="$nextTick(() => {
+        const refreshOptions = () => {
+            // Filter out the selected options from the options list
+            $data.filteredOptions = $data.options.filter(option => !$data.selection.includes(option.value));
+    
+            $el.choices.clearStore();
+            $el.choices.setChoices($data.filteredOptions);
+        }
+    
+        $data.onSelectionChange = () => {
+            // Notify wired model
+            $dispatch('input', $data.selection);
+    
             refreshOptions();
-        });
-    "
+        }
+    
+        $data.addItem = (e) => {
+            // Merge newValue with current selection
+            $data.selection = [...$data.selection, ...$el.choices.getValue(true)];
+            $data.onSelectionChange();
+        }
+    
+        refreshOptions();
+    });"
     x-multiselect="{
         selectEl: $refs.selectEl,
         options: {
@@ -94,27 +89,35 @@
             },
         }
     }">
-
     <template x-for="(option, index) in selection" :key="option">
         <input type="hidden" name="{{ $name }}[]" x-bind:value="option" />
     </template>
-
-    <ol x-sortable x-on:end.stop="sortSelection($event.target.sortable.toArray())">
-        <template x-for="(option, index) in selection" :key="option">
-            <li :x-sortable-item="option">
-                <span x-sortable-handle>PULL</span>
-                <span x-text="index"></span>
-                <span x-text="option"></span>
-                <span x-on:click="removeItem(option)">delete</span>
-            </li>
-        </template>
-    </ol>
 
     <select
         name="{{ $name }}"
         x-ref="selectEl"
         x-on:change="addItem"
-        multiple
-    ></select>
+        multiple></select>
 
+    <ol x-sortable x-on:end.stop="sortSelection($event.target.sortable.toArray())" class="border rounded-lg border-grey-200 mt-3">
+        <template x-for="(option, index) in selection" :key="option">
+            <li :x-sortable-item="option" class="py-2 px-1.5 flex gap-2" :class="{ 'border-t border-grey-200': index > 0 }">
+                <button type="button" x-sortable-handle class="shrink-0">
+                    <svg class="w-5 h-5 text-black" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 256 256">
+                        <path d="M104,60A12,12,0,1,1,92,48,12,12,0,0,1,104,60Zm60,12a12,12,0,1,0-12-12A12,12,0,0,0,164,72ZM92,116a12,12,0,1,0,12,12A12,12,0,0,0,92,116Zm72,0a12,12,0,1,0,12,12A12,12,0,0,0,164,116ZM92,184a12,12,0,1,0,12,12A12,12,0,0,0,92,184Zm72,0a12,12,0,1,0,12,12A12,12,0,0,0,164,184Z"></path>
+                    </svg>
+                </button>
+
+                {{-- <span x-text="index"></span> --}}
+
+                <span x-text="getOptionLabel(option)" class="body-dark body leading-5 grow"></span>
+
+                <button type="button" x-on:click="removeItem(option)" class="shrink-0">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                        <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
+                    </svg>
+                </button>
+            </li>
+        </template>
+    </ol>
 </div>
