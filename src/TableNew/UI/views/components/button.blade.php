24e10bd4dc2510ce3a60230f19292291c0146452
Tijs <tijs@thinktomorrow.be>
2024-06-21 16:59:45 +0200

    Change: button primary/secondary/tertiary styling

--- src/TableNew/UI/views/components/button.blade.php
+++ src/TableNew/UI/views/components/button.blade.php
@@ -1,31 +1,34 @@
 @props([
     'size' => 'base',
     'color' => 'grey',
-    'icon' => null,
-    'iconPosition' => 'left',
+    'iconLeft' => null,
+    'iconRight' => null,
 ])
 
 <span
     {{
         $attributes->class([
-            'bui-btn font-medium shadow-sm ring-1 ring-black/5 hover:shadow hover:ring-black/10',
-            'flex-row-reverse' => $iconPosition === 'right',
-            'bg-grey-100 text-grey-800' => $color === 'grey',
-            'bg-primary-500 text-white' => $color === 'primary',
-            'bg-white text-grey-800' => $color === 'white',
+            'bui-btn font-medium',
+            'bg-grey-100 text-grey-800 hover:bg-grey-200' => $color === 'grey',
+            'bg-primary-500 text-white hover:bg-primary-600' => $color === 'primary',
+            'bg-white text-grey-800 ring-1 ring-grey-100 hover:ring-grey-200' => $color === 'white',
             'bui-btn-base' => $size === 'base',
             'bui-btn-sm' => $size === 'sm',
             'bui-btn-xs' => $size === 'xs',
         ])
     }}
 >
-    @if ($icon)
-        {!! $icon !!}
+    @if ($iconLeft)
+        {!! $iconLeft !!}
     @endif
 
     @if ($slot->isNotEmpty())
         <span class="bui-btn-content">
             {{ $slot }}
         </span>
+    @endif
+
+    @if ($iconRight)
+        {!! $iconRight !!}
     @endif
 </span>
--- src/TableNew/UI/views/livewire/_partials/bulk-actions.blade.php
+++ src/TableNew/UI/views/livewire/_partials/bulk-actions.blade.php
@@ -35,9 +35,8 @@
     <button type="button">
         <x-chief-table-new::button
             size="sm"
-            color="grey"
-            icon='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" color="#000000" fill="none"> <path d="M11.992 12H12.001" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" /> <path d="M11.9842 18H11.9932" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" /> <path d="M11.9998 6H12.0088" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" /> </svg>'
-            icon-position="right"
+            color="white"
+            iconRight='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" color="#000000" fill="none"> <path d="M11.992 12H12.001" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" /> <path d="M11.9842 18H11.9932" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" /> <path d="M11.9998 6H12.0088" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" /> </svg>'
         />
     </button>
 </div>
