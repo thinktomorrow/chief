23e24dbd632b59b5177d49526190512ce67d336d
Tijs <tijs@thinktomorrow.be>
2024-10-14 15:50:02 +0200

    Change: choices.css select icon

--- src/Table/Actions/Presets/VisitArchiveAction.php
+++ src/Table/Actions/Presets/VisitArchiveAction.php
@@ -9,10 +9,12 @@ class VisitArchiveAction extends Action
 {
     public static function makeDefault(string $resourceKey): static
     {
+        $resource = app(Registry::class)->resource($resourceKey);
         $manager = app(Registry::class)->manager($resourceKey);
 
         return static::make('archive-index')
-            ->label("Bekijk archief")
+            ->label('Archief bekijken')
+            ->description('In het archief kan je alle gearchiveerde ' . $resource->getPluralLabel() . ' terugvinden.')
             ->prependIcon('<x-chief::icon.archive />')
             ->link($manager->route('archive_index'))
             ->hidden();
