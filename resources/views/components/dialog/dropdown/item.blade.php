cd727c4cec327f1eae1b14fce7f8614ea079b63c
Tijs <tijs@thinktomorrow.be>
2024-10-14 14:56:31 +0200

    Add: dropdown-item description + info/danger variants

--- resources/views/components/dialog/dropdown/item.blade.php
+++ resources/views/components/dialog/dropdown/item.blade.php
@@ -6,15 +6,16 @@
 @php
     $attributes = $attributes->class([
         'bui-dropdown-item cursor-pointer font-medium',
-        match ($variant) {
-            'default' => 'bui-dropdown-item-default',
-            //'danger' => 'bui-dropdown-item-danger',
-            default => 'bui-dropdown-item-default',
-        },
         match ($size) {
             'base' => 'bui-dropdown-item-base',
             //'sm' => 'bui-dropdown-item-sm',
             default => 'bui-dropdown-item-base',
+        },
+        match ($variant) {
+            'default' => 'bui-dropdown-item-default',
+            'info' => 'bui-dropdown-item-info',
+            'danger' => 'bui-dropdown-item-danger',
+            default => 'bui-dropdown-item-default',
         },
     ]);
 @endphp
