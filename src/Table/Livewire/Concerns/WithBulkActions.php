4f4ab70930d622023d458436b63429fd1ed76fc2
Tijs <tijs@thinktomorrow.be>
2024-11-20 11:32:38 +0100

    Fix: Table last child rounded actions

--- src/Table/Livewire/Concerns/WithBulkActions.php
+++ src/Table/Livewire/Concerns/WithBulkActions.php
@@ -9,23 +9,23 @@ trait WithBulkActions
     /** @return Action[] */
     public function getPrimaryBulkActions(): array
     {
-        return array_filter($this->getTable()->getBulkActions(), fn (Action $action) => $action->isPrimary());
+        return array_filter($this->getTable()->getBulkActions(), fn(Action $action) => $action->isPrimary());
     }
 
     /** @return Action[] */
     public function getSecondaryBulkActions(): array
     {
-        return array_filter($this->getTable()->getBulkActions(), fn (Action $action) => $action->isSecondary());
+        return array_filter($this->getTable()->getBulkActions(), fn(Action $action) => $action->isSecondary());
     }
 
     /** @return Action[] */
     public function getTertiaryBulkActions(): array
     {
-        return array_filter($this->getTable()->getBulkActions(), fn (Action $action) => $action->isTertiary());
+        return array_filter($this->getTable()->getBulkActions(), fn(Action $action) => $action->isTertiary());
     }
 
     public function hasAnyBulkActions(): bool
     {
-        return count($this->getTable()->getBulkActions()) > 0;
+        return count($this->getResults()) > 0 && count($this->getTable()->getBulkActions()) > 0;
     }
 }
