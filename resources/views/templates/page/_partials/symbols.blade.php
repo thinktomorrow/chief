9474a02f6b9c1aed7dfe663c75fe3df015281bb8
Tijs <tijs@thinktomorrow.be>
2025-01-15 16:43:38 +0100

    Cut: paperclip icon

--- resources/views/templates/page/_partials/symbols.blade.php
+++ resources/views/templates/page/_partials/symbols.blade.php
@@ -287,21 +287,6 @@
             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
         </symbol>
         <symbol
-            id="icon-paper-clip"
-            width="100%"
-            height="100%"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-        >
-            <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13"
-            />
-        </symbol>
-        <symbol
             id="icon-squares-plus"
             width="100%"
             height="100%"
