ae07872b8c5ddf42696838beb196e91fcd1cf2b6
BenCavens <497668+BenCavens@users.noreply.github.com>
2025-03-12 09:27:47 +0000

    Pint formatting

--- src/Forms/Fields/Component.php
+++ src/Forms/Fields/Component.php
@@ -48,13 +48,13 @@ abstract class Component extends \Illuminate\View\Component implements Htmlable,
     use HasElementId;
     use HasFieldToggle;
     use HasId;
+
     // Field concerns
     use HasKey;
-
     use HasLabel;
+
     // Generic component concerns
     use HasLocalizableProperties;
-
     use HasModel;
     use HasModelValuePreparation;
     use HasName;
