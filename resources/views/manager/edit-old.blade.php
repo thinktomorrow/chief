b7dcb28219da055c75f345f09b9e39ab02b9273e
Tijs Verellen <tijs@thinktomorrow.be>
2025-03-17 14:59:18 +0100

    Fix: edit-old version

---
+++ resources/views/manager/edit-old.blade.php
@@ -0,0 +1,44 @@
+<x-chief::page.template :title="$resource->getPageTitle($model)">
+    <x-slot name="hero">
+        <x-chief::page.hero :breadcrumbs="[$resource->getPageBreadCrumb('edit')]">
+            @if ($forms->has('pagetitle'))
+                <x-slot name="customTitle">
+                    <x-chief-form::forms id="pagetitle" />
+                </x-slot>
+            @else
+                <x-slot name="title">
+                    {{ $resource->getPageTitle($model) }}
+                </x-slot>
+            @endif
+
+            @if (count(config('chief.locales')) > 1)
+                <x-chief::tabs :listen-for-external-tab="true" class="-mb-3 mt-1">
+                    @foreach (config('chief.locales') as $locale)
+                        <x-chief::tabs.tab tab-id="{{ $locale }}"></x-chief::tabs.tab>
+                    @endforeach
+                </x-chief::tabs>
+            @endif
+
+            @include('chief::manager._partials.edit-actions')
+        </x-chief::page.hero>
+    </x-slot>
+
+    <x-chief::page.grid>
+        <x-chief-form::forms position="main" />
+
+        @adminCan('fragments-index', $model)
+        <x-chief::fragments :owner="$model" />
+        @endAdminCan
+
+        <x-chief-form::forms position="main-bottom" />
+
+        <x-slot name="aside">
+            <x-chief-form::forms position="aside-top" />
+            <x-chief::window.states />
+            <x-chief::window.links />
+            <x-chief-form::forms position="aside" />
+        </x-slot>
+    </x-chief::page.grid>
+
+    @include('chief::templates.page._partials.editor-script')
+</x-chief::page.template>
