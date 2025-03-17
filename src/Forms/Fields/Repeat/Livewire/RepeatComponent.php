2ab159cefe6016bb9cef89ab3dbf307a5b74a0cf
Ben Cavens <cavensben@gmail.com>
2025-03-17 14:09:38 +0100

    WIP repeat field to livewire

---
+++ src/Forms/Fields/Repeat/Livewire/RepeatComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Thinktomorrow\Chief\Forms\Fields\Repeat\Livewire;
+
+use Livewire\Component;
+
+class RepeatComponent extends Component
+{
+    public function render()
+    {
+        return view('chief-forms::livewire.repeat');
+    }
+}
