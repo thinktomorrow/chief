3de4e9f3a271e6629e44d0e05e7a46af47a2f15d
Ben Cavens <cavensben@gmail.com>
2025-04-03 11:04:26 +0200

    Add: model contract

--- src/Models/ModelDefaults.php
+++ src/Models/ModelDefaults.php
@@ -2,24 +2,33 @@
 
 namespace Thinktomorrow\Chief\Models;
 
+use Thinktomorrow\AssetLibrary\InteractsWithAssets;
+use Thinktomorrow\Chief\ManagedModels\States\SimpleState\UsesSimpleState;
 use Thinktomorrow\Chief\Resource\ResourceKeyFormat;
 use Thinktomorrow\Chief\Shared\ModelReferences\ReferableModelDefault;
+use Thinktomorrow\Chief\Sites\Locales\ChiefLocales;
 use Thinktomorrow\DynamicAttributes\HasDynamicAttributes;
 
 trait ModelDefaults
 {
     use HasDynamicAttributes;
+    use InteractsWithAssets;
     use ReferableModelDefault;
+    use UsesSimpleState;
 
-    /**
-     * This is an optional method for the DynamicAttributes behavior and allows for
-     * proper localized values to be returned. Here we provide the default in
-     * advance in case the model decides to make use of DynamicAttributes.
-     */
-    public function getDynamicLocales(): array
+    protected function getDynamicLocales(): array
     {
-        // TODO: get only the locales that are used by this model
         return ChiefLocales::locales();
+    }
+
+    protected function getDynamicFallbackLocales(): array
+    {
+        return ChiefLocales::fallbackLocales();
+    }
+
+    protected function getAssetFallbackLocales(): array
+    {
+        return ChiefLocales::fallbackLocales();
     }
 
     public function viewKey(): string
