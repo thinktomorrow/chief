e96f403da254e0f7529a627734a0df83a77be0a7
Tijs Verellen <tijs@thinktomorrow.be>
2025-04-23 14:49:23 +0200

    Fix: file-field-upload modals opening inside of drawer

--- src/Assets/App/resources/file-field-upload.blade.php
+++ src/Assets/App/resources/file-field-upload.blade.php
@@ -13,29 +13,29 @@
 
     {{ $this->fileSelect }}
 
-    <div>
+    <template x-teleport="body">
         <livewire:chief-wire::file-field-choose
             parent-id="{{ $this->getId() }}"
             allowMultiple="{{ $allowMultiple }}"
         />
-    </div>
+    </template>
 
-    <div>
+    <template x-teleport="body">
         <livewire:chief-wire::file-field-choose-external
             parent-id="{{ $this->getId() }}"
             allowMultiple="{{ $allowMultiple }}"
         />
-    </div>
+    </template>
 
-    <div>
+    <template x-teleport="body">
         <livewire:chief-wire::file-field-edit
-            parent-id="{{ $this->getId() }}"
-            model-reference="{{ $modelReference }}"
-            field-key="{{ $fieldKey }}"
-            locale="{{ $locale }}"
+            :parent-id="$this->getId()"
+            :model-reference="$modelReference"
+            :field-key="$fieldKey"
+            :locale="$locale"
             :components="$this->components"
         />
-    </div>
+    </template>
 
     @foreach (app(ChiefPluginSections::class)->getLivewireFileComponents() as $livewireFileComponent)
         <div>
