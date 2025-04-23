35168cb27549a4b6d12416dd9cc2562f1277ca65
Ben Cavens <cavensben@gmail.com>
2025-04-23 16:46:34 +0200

    Fix: tabs

--- src/Fragments/UI/views/livewire/_partials/edit-fragment-tabs.blade.php
+++ src/Fragments/UI/views/livewire/_partials/edit-fragment-tabs.blade.php
@@ -5,7 +5,7 @@
                 aria-controls="{{ $locale }}"
                 aria-selected="{{ $locale === $scopedLocale ? 'true' : 'false' }}"
                 wire:key="fragment-site-toggle-{{ $locale }}"
-                wire:click="onScopedToLocale('{{ $locale }}')"
+                x-on:click="$dispatch('chieftab', { id: '{{ $locale }}' }); $wire.onScopedToLocale('{{ $locale }}')"
                 :active="$locale == $scopedLocale"
             >
                 {{ \Thinktomorrow\Chief\Sites\ChiefSites::name($locale) }}
