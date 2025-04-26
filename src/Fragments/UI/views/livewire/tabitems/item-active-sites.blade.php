8f61b3a6d85f2297b7b7b6b7091c86b4a3ea6928
Ben Cavens <cavensben@gmail.com>
2025-04-26 20:49:42 +0200

    context text stuff

--- src/Fragments/UI/views/livewire/tabitems/item-active-sites.blade.php
+++ src/Fragments/UI/views/livewire/tabitems/item-active-sites.blade.php
@@ -7,49 +7,58 @@
                 @if (($this->getItem()->exists() && in_array($locale, $form['locales'])) || in_array($locale, $form['active_sites']))
                     <div class="flex items-start justify-end gap-1">
                         @if (in_array($locale, $this->getItem()->getActiveSites()))
-                            <div class="flex gap-2 items-center">
-                                <x-chief::icon.link class="size-4 text-grey-500" />
-                                <div class="flex flex-col text-sm text-grey-500">
-                                    <span class="body-dark font-medium">
-                                        {{ \Thinktomorrow\Chief\Sites\ChiefSites::name($locale) }}
-                                    </span>
-                                    <span>
-                                        Deze versie toont op de {{ \Thinktomorrow\Chief\Sites\ChiefSites::adjective($locale) }} site.  Om een andere versie te tonen, activeer in die versie de {{ \Thinktomorrow\Chief\Sites\ChiefSites::adjective($locale) }} site.
-                                    </span>
+                            <div class="flex flex-col text-sm text-grey-500">
+                                <div class="body-dark font-medium flex gap-2">
+                                    <x-chief::icon.link class="size-4 text-grey-500" />
+                                    {{ \Thinktomorrow\Chief\Sites\ChiefSites::name($locale) }}
                                 </div>
+                                <span>
+                                    Deze versie toont op de {{ \Thinktomorrow\Chief\Sites\ChiefSites::adjective($locale) }} site.  Om een andere versie te tonen, activeer de {{ \Thinktomorrow\Chief\Sites\ChiefSites::adjective($locale) }} site in die andere versie .
+                                </span>
                             </div>
 
                         @elseif (! in_array($locale, $form['active_sites']))
-                            <x-chief::button
-                                type="button"
-                                size="xs"
-                                variant="blue"
-                                tabindex="-1"
-                                x-on:click="$wire.addActiveSite('{{ $locale }}')"
-                            >
-                                <x-chief::icon.link />
-                                <span>
-                                    Toon deze versie op de {{ \Thinktomorrow\Chief\Sites\ChiefSites::adjective($locale) }}
-                                site
-                                </span>
-
-                            </x-chief::button>
+                            <div class="flex flex-col gap-2 text-sm text-grey-500">
+                                <div class="body-dark font-medium flex gap-2">
+                                    <x-chief::icon.unlink class="size-4 text-grey-500" />
+                                    {{ \Thinktomorrow\Chief\Sites\ChiefSites::name($locale) }}
+                                </div>
+                                <x-chief::button
+                                    type="button"
+                                    size="xs"
+                                    variant="blue"
+                                    tabindex="-1"
+                                    x-on:click="$wire.addActiveSite('{{ $locale }}')"
+                                >
+                                    <span>
+                                        Toon deze versie op de {{ \Thinktomorrow\Chief\Sites\ChiefSites::adjective($locale) }}
+                                    site
+                                    </span>
+                                </x-chief::button>
+                            </div>
                         @else
-                            <div class="flex items-start gap-1">
+                            <div class="flex flex-col gap-2 text-sm text-grey-500">
+                                <div class="body-dark font-medium flex gap-2">
+                                    <x-chief::icon.link class="size-4 text-grey-500" />
+                                    {{ \Thinktomorrow\Chief\Sites\ChiefSites::name($locale) }}
+                                </div>
+                                <div class="flex items-start gap-1">
                                 <span class="text-sm leading-6 text-grey-500">
                                     Deze versie zal na bewaren tonen op de {{ \Thinktomorrow\Chief\Sites\ChiefSites::adjective($locale) }} site.
                                 </span>
 
-                                <x-chief::button
-                                    type="button"
-                                    size="xs"
-                                    variant="grey"
-                                    tabindex="-1"
-                                    x-on:click="$wire.removeActiveSite('{{ $locale }}')"
-                                >
-                                    <x-chief::icon.arrow-turn-backward />
-                                </x-chief::button>
+                                    <x-chief::button
+                                        type="button"
+                                        size="xs"
+                                        variant="grey"
+                                        tabindex="-1"
+                                        x-on:click="$wire.removeActiveSite('{{ $locale }}')"
+                                    >
+                                        <x-chief::icon.arrow-turn-backward />
+                                    </x-chief::button>
+                                </div>
                             </div>
+
                         @endif
                     </div>
                 @endif
