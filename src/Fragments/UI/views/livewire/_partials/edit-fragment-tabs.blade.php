27eea460e78705c3fd0fd6d6dfba3849ba98dd6c
Tijs Verellen <tijs@thinktomorrow.be>
2025-04-29 11:35:29 +0200

    Fix: edit-form-tabs + grid layout

--- src/Fragments/UI/views/livewire/_partials/edit-fragment-tabs.blade.php
+++ src/Fragments/UI/views/livewire/_partials/edit-fragment-tabs.blade.php
@@ -1,24 +1,29 @@
-<x-chief::button-group data-slot="form-group" size="base">
-    @foreach ($locales as $locale)
-        <x-chief::button-group.button
-            :aria-controls="$locale"
-            :aria-selected="$locale === $scopedLocale ? 'true' : 'false'"
-            wire:key="fragment-site-toggle-{{ $locale }}"
-            x-on:click="$dispatch('chieftab', { id: '{{ $locale }}', reference: 'fragment-site-toggle' }); $wire.onScopedToLocale('{{ $locale }}')"
-        >
-            {{ \Thinktomorrow\Chief\Sites\ChiefSites::name($locale) }}
-        </x-chief::button-group.button>
-    @endforeach
-</x-chief::button-group>
+<div data-slot="form-group">
+    <x-chief::button-group size="base">
+        @foreach ($locales as $locale)
+            <x-chief::button-group.button
+                :aria-controls="$locale"
+                :aria-selected="$locale === $scopedLocale ? 'true' : 'false'"
+                wire:key="fragment-site-toggle-{{ $locale }}"
+                x-on:click="$dispatch('chieftab', { id: '{{ $locale }}', reference: 'fragment-site-toggle' }); $wire.onScopedToLocale('{{ $locale }}')"
+            >
+                {{ \Thinktomorrow\Chief\Sites\ChiefSites::name($locale) }}
+            </x-chief::button-group.button>
+        @endforeach
+    </x-chief::button-group>
 
-{{-- Hack to get the scoped locale tab to be active when opening the drawer --}}
-<div
-    x-data="{
-        scopedLocale: '{{ $scopedLocale }}',
-        init() {
-            this.$nextTick(() => {
-                this.$dispatch('chieftab', { id: '{{ $scopedLocale }}', reference: 'fragment-site-toggle' })
-            })
-        },
-    }"
-></div>
+    {{-- Hack to get the scoped locale tab to be active when opening the drawer --}}
+    <div
+        x-data="{
+            scopedLocale: '{{ $scopedLocale }}',
+            init() {
+                this.$nextTick(() => {
+                    this.$dispatch('chieftab', {
+                        id: '{{ $scopedLocale }}',
+                        reference: 'fragment-site-toggle',
+                    })
+                })
+            },
+        }"
+    ></div>
+</div>
