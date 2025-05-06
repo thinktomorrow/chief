4e396879b4da7f8f706fd0455c0afea6f5a67ab0
Ben Cavens <cavensben@gmail.com>
2025-05-06 10:02:59 +0200

    Form tests

--- src/Forms/Tests/Layouts/CustomizingFormViewTest.php
+++ src/Forms/Tests/Layouts/CustomizingFormViewTest.php
@@ -7,18 +7,8 @@ namespace Thinktomorrow\Chief\Forms\Tests\Layouts;
 use Thinktomorrow\Chief\Forms\Layouts\Form;
 use Thinktomorrow\Chief\Forms\Tests\FormsTestCase;
 
-class FormTest extends FormsTestCase
+class CustomizingFormViewTest extends FormsTestCase
 {
-    public function test_it_can_set_form_component()
-    {
-        $component = Form::make('general')->components([
-            new Textarea('intro'),
-        ]);
-
-        $this->assertCount(1, $component->getComponents());
-        $this->assertIsString($component->toHtml());
-    }
-
     public function test_it_can_have_a_custom_view()
     {
         $this->app['view']->addNamespace('test-views', __DIR__.'/../TestSupport/stubs/views');
