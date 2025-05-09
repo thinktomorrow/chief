cd45794b641f14b8395bd70e277fcd120175a5bd
TijsTT <43745131+TijsTT@users.noreply.github.com>
2025-05-09 13:52:08 +0000

    Pint formatting

--- src/Forms/Fields/Editor/Extensions/Extension.php
+++ src/Forms/Fields/Editor/Extensions/Extension.php
@@ -8,26 +8,26 @@ interface Extension
 {
     /**
      * The button is rendered in a toolbar above the editor. This should trigger the main action of the extension.
-     * @return View|null
      */
     public static function renderButton(?array $viewData = []): ?View;
 
     /**
      * The footer is rendered below the editor.
      * Can be used to add additional HTML, e.g. the character count element.
-     * @return View|null
      */
     public static function renderFooter(): ?View;
 
     /**
      * The names of the necessary TipTap JavaScript extensions for this to work.
      * These will be loaded when the editor is initialized.
+     *
      * @return string
      */
     public static function jsExtensions(): array;
 
     /**
      * The roles that are required to use this extension, e.g. you need role 'dev' to use the html extension.
+     *
      * @return string
      */
     public static function roles(): array;
