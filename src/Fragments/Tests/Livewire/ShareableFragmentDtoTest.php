ea0e7541b403bd4e842130e8bfc19822558339f7
Ben Cavens <cavensben@gmail.com>
2025-05-11 08:01:07 +0200

    Fix

--- src/Fragments/Tests/Livewire/ShareableFragmentDtoTest.php
+++ src/Fragments/Tests/Livewire/ShareableFragmentDtoTest.php
@@ -35,7 +35,7 @@ class ShareableFragmentDtoTest extends ChiefTestCase
         $context2 = FragmentTestHelpers::createContext($owner2);
         FragmentTestHelpers::attachFragment($context2->id, $fragment->getFragmentId());
 
-        $sharedFragmentDtos = $this->composer->getSharedFragmentDtos($fragment->getFragmentId(), $this->owner);
+        $sharedFragmentDtos = $this->composer->getSharedFragmentDtos($fragment->getFragmentId(), $this->owner->modelReference());
 
         $this->assertCount(2, $sharedFragmentDtos);
 
