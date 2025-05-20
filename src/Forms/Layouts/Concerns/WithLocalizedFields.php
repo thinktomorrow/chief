ff3f1da00a18c6e9c394f6a19fe9de78847ef1fc
Ben Cavens <cavensben@gmail.com>
2025-05-20 11:36:00 +0200

    Fix: file localization exception

--- src/Forms/Layouts/Concerns/WithLocalizedFields.php
+++ src/Forms/Layouts/Concerns/WithLocalizedFields.php
@@ -7,7 +7,7 @@ trait WithLocalizedFields
     public function setScopedLocale(string $locale): self
     {
         foreach ($this->getAllFields() as $field) {
-            if ($field->hasLocales()) {
+            if ($field->showsLocales()) {
                 $field->setLocales([$locale]);
             }
         }
@@ -18,7 +18,7 @@ trait WithLocalizedFields
     public function setLocales(array $locales): self
     {
         foreach ($this->getAllFields() as $field) {
-            if ($field->hasLocales()) {
+            if ($field->showsLocales()) {
                 $field->setLocales($locales);
             }
         }
