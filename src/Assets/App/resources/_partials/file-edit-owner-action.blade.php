20993172006e78bba7552df5cc43fd2d62defe7e
Ben Cavens <cavensben@gmail.com>
2025-06-27 09:35:53 +0200

    Bit simplifying UI

--- src/Assets/App/resources/_partials/file-edit-owner-action.blade.php
+++ src/Assets/App/resources/_partials/file-edit-owner-action.blade.php
@@ -9,67 +9,54 @@
 @endif
 
 @teleport('body')
-    <x-chief::dialog.modal
-        id="file-owners-modal-{{ $this->getId() }}"
-        title="Dit bestand wordt gebruikt op {{ $ownerCount == 1 ? ' één plaats' : $ownerCount . ' plaatsen' }}"
-    >
-        <p class="body text-grey-500">
-            Het aanpassen of vervangen van dit bestand is van toepassing op volgende
-            {{ $ownerCount == 1 ? 'plaats' : 'plaatsen' }}:
-        </p>
-
-        <div class="mt-2 overflow-hidden rounded-md border border-grey-100">
-            <div class="max-h-48 divide-y divide-grey-100 overflow-y-auto">
-                @foreach ($previewFile->owners as $owner)
-                    <div class="flex items-start justify-between gap-3 px-3 py-2.5">
-                        <div class="body-dark body leading-6">
-                            @if (isset($modelReference) && $owner['modelReference'] == $modelReference)
-                                <span class="font-bold">{{ $owner['label'] }}</span>
-                                <span class="label label-xs label-grey">Deze koppeling</span>
-                            @else
-                                {{ $owner['label'] }}
-                            @endif
-                        </div>
-
-                        @if (! isset($modelReference) || $owner['modelReference'] != $modelReference)
-                            <x-chief::link
+<x-chief::dialog.modal
+    id="file-owners-modal-{{ $this->getId() }}"
+    title="Dit bestand wordt op {{ $ownerCount == 1 ? ' één plaats' : $ownerCount . ' plaatsen' }} gebruikt"
+>
+    <div class="mt-2 overflow-hidden rounded-md border border-grey-100">
+        <div class="max-h-48 divide-y divide-grey-100 overflow-y-auto">
+            @foreach ($previewFile->owners as $owner)
+                <div class="flex items-start justify-between gap-3 px-3 py-2.5">
+                    <div class="flex justify-between w-full body-dark body leading-6">
+                        <span>{{ teaser($owner['label'], 30, '...') }}</span>
+                        @if (isset($modelReference) && $owner['modelReference'] == $modelReference)
+                            <x-chief::button
+                                variant="blue"
+                                type="button"
+                                size="xs"
+                                x-on:click.stop="close(); $wire.isolateAsset()">
+                                <x-chief::icon.unlink />
+                                <span>Afzonderlijk bewerken op deze pagina</span>
+                            </x-chief::button>
+                        @else
+                            <x-chief::button
                                 href="{{ $owner['adminUrl'] }}"
                                 title="Bekijk"
+                                size="xs"
+                                variant="grey"
                                 target="_blank"
                                 rel="noopener"
                             >
                                 <x-chief::icon.link-square />
-                            </x-chief::link>
+                                <span>Ga naar deze pagina</span>
+                            </x-chief::button>
                         @endif
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
+    </div>
 
-        @if ($currentOwner && $ownerCount > 1)
-            <p class="body mt-4 text-grey-500">
-                Wil je aanpassingen enkel op deze plaats doorvoeren? Kies dan om het bestand te ontkoppelen en apart te
-                bewerken.
-            </p>
-        @endif
+    @if ($currentOwner && $ownerCount > 1)
+        <p class="body mt-4 text-grey-500">
+            Wil je de afbeelding alleen op deze pagina aanpassen? Koppel ze dan los om ze apart te kunnen bewerken.
+        </p>
+    @endif
 
-        <x-slot name="footer">
-            <x-chief::dialog.modal.footer>
-                <x-chief::button x-on:click="close" type="button">Sluit</x-chief::button>
-
-                @if ($currentOwner && $ownerCount > 1)
-                    <x-chief::button
-                        variant="blue"
-                        type="button"
-                        x-on:click.stop="
-                            close()
-                            $wire.isolateAsset()
-                        "
-                    >
-                        Ontkoppel en bewerk apart in {{ $currentOwner['label'] }}
-                    </x-chief::button>
-                @endif
-            </x-chief::dialog.modal.footer>
-        </x-slot>
-    </x-chief::dialog.modal>
+    <x-slot name="footer">
+        <x-chief::dialog.modal.footer>
+            <x-chief::button x-on:click="close" type="button">Sluit</x-chief::button>
+        </x-chief::dialog.modal.footer>
+    </x-slot>
+</x-chief::dialog.modal>
 @endteleport
