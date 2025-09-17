068902950dec4074ebd8ec60647d02597c8f8283
Tijs Verellen <tijs@thinktomorrow.be>
2025-09-17 11:58:55 +0200

    Change: tags table also using Basic Table Components

--- src/Plugins/Tags/App/resources/views/tags/index.blade.php
+++ src/Plugins/Tags/App/resources/views/tags/index.blade.php
@@ -8,9 +8,9 @@
 
         <x-chief::window>
             @if ($tagGroups->count() > 1)
-                <div id="taggroup-{{ $tagGroup->getTagGroupId() }}" class="mb-2 flex items-start justify-between">
+                <div id="taggroup-{{ $tagGroup->getTagGroupId() }}" class="mb-3 flex items-start justify-between">
                     <div class="flex items-center gap-2">
-                        <span class="body text-grey-500">
+                        <span class="font-display text-grey-950 text-xl/6 font-semibold">
                             {{ ucfirst($tagGroup->getLabel()) }}
                         </span>
 
@@ -43,68 +43,60 @@
                 </div>
             @endif
 
-            <div class="rounded-xl border border-grey-200">
-                <table class="min-w-full table-fixed divide-y divide-grey-200">
-                    <tbody class="divide-y divide-grey-200">
-                        @forelse ($tagsByGroup as $tag)
-                            <tr>
-                                <td class="py-1.5 pl-3 text-left">
-                                    <div class="flex min-h-6 items-center gap-1.5">
-                                        <a
-                                            href="{{ route('chief.tags.edit', $tag->getTagId()) }}"
-                                            title="{{ $tag->getLabel() }}"
-                                            class="inline-flex items-center gap-x-3"
-                                        >
-                                            <svg
-                                                class="h-2.5 w-2.5"
-                                                style="fill: {{ $tag->getColor() }}"
-                                                viewBox="0 0 6 6"
-                                                aria-hidden="true"
-                                            >
-                                                <circle cx="3" cy="3" r="3" />
-                                            </svg>
+            <x-chief::table variant="transparent">
+                <x-chief::table.body>
+                    @forelse ($tagsByGroup as $tag)
+                        <x-chief::table.row>
+                            <x-chief::table.cell>
+                                <a
+                                    href="{{ route('chief.tags.edit', $tag->getTagId()) }}"
+                                    title="{{ $tag->getLabel() }}"
+                                    class="inline-flex items-center gap-x-3"
+                                >
+                                    <svg
+                                        class="h-2.5 w-2.5"
+                                        style="fill: {{ $tag->getColor() }}"
+                                        viewBox="0 0 6 6"
+                                        aria-hidden="true"
+                                    >
+                                        <circle cx="3" cy="3" r="3" />
+                                    </svg>
 
-                                            <span class="body-dark font-medium hover:underline">
-                                                {{ $tag->getLabel() }}
-                                            </span>
-                                        </a>
-                                    </div>
-                                </td>
-                                <td class="py-1.5 pl-3 text-left">
-                                    <div class="flex min-h-6 items-center gap-1.5 text-sm text-grey-500">
-                                        @if ($tag->getUsages() > 0)
-                                            <p>In gebruik op {{ $tag->getUsages() }} pagina's</p>
-                                        @else
-                                            <p>Niet in gebruik</p>
-                                        @endif
-                                    </div>
-                                </td>
-                                <td class="py-1.5 pr-3 text-right">
-                                    <div class="-mr-2 flex justify-end gap-2">
-                                        <x-chief::button
-                                            href="{{ route('chief.tags.edit', $tag->getTagId()) }}"
-                                            variant="grey"
-                                            size="sm"
-                                        >
-                                            <x-chief::icon.quill-write />
-                                        </x-chief::button>
+                                    <span class="body-dark font-medium hover:underline">
+                                        {{ $tag->getLabel() }}
+                                    </span>
+                                </a>
+                            </x-chief::table.cell>
+                            <x-chief::table.cell>
+                                @if ($tag->getUsages() > 0)
+                                    <p>In gebruik op {{ $tag->getUsages() }} pagina's</p>
+                                @else
+                                    <p>Niet in gebruik</p>
+                                @endif
+                            </x-chief::table.cell>
+                            <x-chief::table.cell justify="end">
+                                <x-chief::button
+                                    href="{{ route('chief.tags.edit', $tag->getTagId()) }}"
+                                    variant="grey"
+                                    size="sm"
+                                >
+                                    <x-chief::icon.quill-write />
+                                </x-chief::button>
 
-                                        @include('chief-tags::tags.delete')
-                                    </div>
-                                </td>
-                            </tr>
-                        @empty
-                            <tr>
-                                <td class="py-1.5 pl-3 text-left">
-                                    <div class="flex min-h-[30px] items-center">
-                                        <span class="text-grey-400">Deze groep bevat nog geen tags</span>
-                                    </div>
-                                </td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                </table>
-            </div>
+                                @include('chief-tags::tags.delete')
+                            </x-chief::table.cell>
+                        </x-chief::table.row>
+                    @empty
+                        <x-chief::table.row>
+                            <x-chief::table.cell>
+                                <div class="flex min-h-[30px] items-center">
+                                    <span class="text-grey-400">Deze groep bevat nog geen tags</span>
+                                </div>
+                            </x-chief::table.cell>
+                        </x-chief::table.row>
+                    @endforelse
+                </x-chief::table.body>
+            </x-chief::table>
         </x-chief::window>
     @endforeach
 
@@ -117,7 +109,7 @@
         </div>
     @else
         <div class="space-y-4 py-4">
-            <h2 class="h6 h6-dark">Nog geen enkele tag te zien... 😭</h2>
+            <h2 class="font-display text-grey-950 text-xl/6 font-semibold">Nog geen enkele tag te zien... 😭</h2>
 
             <div>
                 <x-chief::button href="{{ route('chief.tags.create') }}" title="Tag toevoegen">
@@ -126,7 +118,7 @@
                 </x-chief::button>
             </div>
 
-            <p class="body text-sm text-grey-500">
+            <p class="body text-grey-500 text-sm">
                 Tags houden jouw paginabeheer overzichtelijk.
                 <br />
                 Gebruik ze om jouw werk te stroomlijnen en het paginaoverzicht te filteren.
