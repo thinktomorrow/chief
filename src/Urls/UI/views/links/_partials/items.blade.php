bd3de2c1f117e891fa800d090206a4cd24204504
Tijs Verellen <tijs@thinktomorrow.be>
2025-11-21 16:05:58 +0100

    Fix: links items redirects don't wrap to next row in flexbox

--- src/Urls/UI/views/links/_partials/items.blade.php
+++ src/Urls/UI/views/links/_partials/items.blade.php
@@ -3,8 +3,11 @@
 @endphp
 
 @foreach ($this->links as $i => $link)
-    <x-chief::callout :title="count($this->links) > 1 ? $link->site->name : ''" wire:key="{{ $link->locale }}"
-                      variant="outline-white">
+    <x-chief::callout
+        :title="count($this->links) > 1 ? $link->site->name : ''"
+        wire:key="{{ $link->locale }}"
+        variant="outline-white"
+    >
         <div
             class="space-y-3"
             x-data="{
@@ -14,13 +17,15 @@
             }"
         >
             <div class="flex items-start justify-between gap-2">
-                <div class="mt-[0.1875rem]">
+                <div class="mt-0.75">
                     @if (! $this->allowedSite($link->locale))
                         <p class="mb-2 text-sm text-red-400">De pagina staat niet langer gepubliceerd op deze site.</p>
                     @endif
 
-                    <p class="my-0.5 text-sm/5 font-medium text-grey-500">
-                        <span x-text="baseUrl"></span>/<span x-text="url.replace(/^\/+/, '')"></span>
+                    <p class="text-grey-500 my-0.5 text-sm/5 font-medium">
+                        <span x-text="baseUrl"></span>
+                        /
+                        <span x-text="url.replace(/^\/+/, '')"></span>
                     </p>
                 </div>
 
@@ -34,7 +39,7 @@
                             <x-chief::icon.arrow-turn-backward />
                             <span>Ongedaan maken</span>
                         </x-chief::button>
-                    @elseif($this->isAllowedToDelete($link))
+                    @elseif ($this->isAllowedToDelete($link))
                         {{-- No badge component here because of data binding that doesn't work with variant --}}
                         <span
                             x-text="status"
@@ -54,7 +59,7 @@
             </div>
 
             @if (! $this->queuedForDeletion($link->locale))
-                <div class="w-full flex justify-between gap-4 space-y-4">
+                <div class="gutter-2 flex flex-wrap items-start justify-start">
                     <div class="w-2/3">
                         <x-chief::form.fieldset rule="form.{{$link->locale}}.slug">
                             <x-chief::form.label for="form.{{$link->locale}}.slug" required>Link</x-chief::form.label>
@@ -76,7 +81,10 @@
                                     x-on:change="status = $event.target.value"
                                 >
                                     @foreach (\Thinktomorrow\Chief\Urls\Models\LinkStatus::options() as $optionValue => $optionLabel)
-                                        <option wire:key="status-option-{{ $optionValue }}" value="{{ $optionValue }}">
+                                        <option
+                                            wire:key="status-option-{{ $optionValue }}"
+                                            value="{{ $optionValue }}"
+                                        >
                                             {{ $optionLabel }}
                                         </option>
                                     @endforeach
@@ -90,45 +98,47 @@
                     @endphp
 
                     @if ($redirectsPerSiteCount > 0)
-                        <x-chief::form.fieldset>
-                            <x-chief::form.label>Redirects</x-chief::form.label>
+                        <div class="w-full">
+                            <x-chief::form.fieldset>
+                                <x-chief::form.label>Redirects</x-chief::form.label>
 
-                            <div
-                                data-slot="control"
-                                class="max-h-48 divide-y divide-grey-100 overflow-y-auto rounded-lg border border-grey-100"
-                            >
-                                @foreach ($redirects->get($link->locale) as $redirect)
-                                    <div class="flex items-start justify-between gap-2 px-2 py-1">
-                                        <div class="mt-[0.1875rem]">
-                                            <p class="body-dark my-0.5 text-sm/5 font-medium">
-                                                {{ $redirect->url->url }}
-                                            </p>
-                                        </div>
+                                <div
+                                    data-slot="control"
+                                    class="divide-grey-100 border-grey-100 max-h-48 divide-y overflow-y-auto rounded-lg border"
+                                >
+                                    @foreach ($redirects->get($link->locale) as $redirect)
+                                        <div class="flex items-start justify-between gap-2 px-2 py-1">
+                                            <div class="mt-0.75">
+                                                <p class="body-dark my-0.5 text-sm/5 font-medium">
+                                                    {{ $redirect->url->url }}
+                                                </p>
+                                            </div>
 
-                                        <div class="flex items-center gap-2">
-                                            @if ($this->redirectQueuedForDeletion($redirect->url->id))
-                                                <x-chief::button
-                                                    x-on:click="$wire.undoDeleteRedirect('{{ $redirect->url->id }}')"
-                                                    variant="grey"
-                                                    size="sm"
-                                                >
-                                                    <x-chief::icon.arrow-turn-backward />
-                                                    <span>Ongedaan maken</span>
-                                                </x-chief::button>
-                                            @else
-                                                <x-chief::button
-                                                    x-on:click="$wire.deleteRedirect('{{ $redirect->url->id }}')"
-                                                    variant="outline-red"
-                                                    size="sm"
-                                                >
-                                                    <x-chief::icon.delete />
-                                                </x-chief::button>
-                                            @endif
+                                            <div class="flex items-center gap-2">
+                                                @if ($this->redirectQueuedForDeletion($redirect->url->id))
+                                                    <x-chief::button
+                                                        x-on:click="$wire.undoDeleteRedirect('{{ $redirect->url->id }}')"
+                                                        variant="grey"
+                                                        size="sm"
+                                                    >
+                                                        <x-chief::icon.arrow-turn-backward />
+                                                        <span>Ongedaan maken</span>
+                                                    </x-chief::button>
+                                                @else
+                                                    <x-chief::button
+                                                        x-on:click="$wire.deleteRedirect('{{ $redirect->url->id }}')"
+                                                        variant="outline-red"
+                                                        size="sm"
+                                                    >
+                                                        <x-chief::icon.delete />
+                                                    </x-chief::button>
+                                                @endif
+                                            </div>
                                         </div>
-                                    </div>
-                                @endforeach
-                            </div>
-                        </x-chief::form.fieldset>
+                                    @endforeach
+                                </div>
+                            </x-chief::form.fieldset>
+                        </div>
                     @endif
                 </div>
             @endif
