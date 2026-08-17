18d9b1abaca947a273e6c6c19b6fb68a817f6074
TijsTT <tijs@thinktomorrow.be>
2026-08-17 11:12:06 +0200

    Fix: formatting

--- resources/views/templates/page/nav/nav-project.blade.php
+++ resources/views/templates/page/nav/nav-project.blade.php
@@ -2,4 +2,4 @@
 {{--<x-chief::nav title="Blog" inline tagged="nav-blog"/>--}}
 {{--<x-chief::nav title="Bedrijf" inline tagged="nav-company"/>--}}
 
-<x-chief::nav tagged="nav" inline/>
+<x-chief::nav tagged="nav" inline />
--- resources/views/templates/page/nav/nav.blade.php
+++ resources/views/templates/page/nav/nav.blade.php
@@ -1,18 +1,18 @@
 {{-- Mobile navigation toggle --}}
 <div class="container max-w-full lg:hidden">
     <div class="-ml-2 flex items-center justify-start pt-6 lg:hidden">
-        <div data-mobile-navigation-toggle class="shrink-0 cursor-pointer rounded-md p-2 hover:bg-grey-200">
-            <x-chief::icon.menu class="size-6 text-grey-700" />
+        <div data-mobile-navigation-toggle class="hover:bg-grey-200 shrink-0 cursor-pointer rounded-md p-2">
+            <x-chief::icon.menu class="text-grey-700 size-6" />
         </div>
 
-        <span class="py-2 font-semibold text-grey-700">Menu</span>
+        <span class="text-grey-700 py-2 font-semibold">Menu</span>
     </div>
 </div>
 
 {{-- Navigation --}}
 <div
     data-mobile-navigation
-    @class([
+    @class ([
         'fixed inset-0 hidden animate-slide-in-nav max-lg:z-10 max-lg:bg-white lg:static lg:flex lg:h-screen lg:animate-none',
         'select-none flex-col justify-between gap-y-9 overflow-y-auto py-6 pl-6 lg:sticky lg:top-0',
     ])
@@ -21,27 +21,21 @@
         {{-- Desktop Chief title --}}
         <div class="hidden items-center justify-start lg:flex">
             <div class="shrink-0 p-2">
-                <x-chief::icon.quill-write class="size-6 text-grey-400" />
+                <x-chief::icon.quill-write class="text-grey-400 size-6" />
             </div>
 
             <a
                 href="{{ route('chief.back.dashboard') }}"
                 title="Ga naar Dashboard"
-                class="block w-full py-2 text-sm leading-6 text-grey-700 hover:text-grey-950"
+                class="text-grey-700 hover:text-grey-950 block w-full py-2 text-sm leading-6"
             >
                 {{ config('app.client', 'Chief') }}
             </a>
         </div>
 
-        {{--
-            <div class="w-full">
-            <livewire:chief-wire::site-toggle />
-            </div>
-        --}}
-
         {{-- Mobile Chief title --}}
         <div class="flex items-center justify-start lg:hidden">
-            <div data-mobile-navigation-toggle class="shrink-0 cursor-pointer rounded-md p-2 hover:bg-grey-50">
+            <div data-mobile-navigation-toggle class="hover:bg-grey-50 shrink-0 cursor-pointer rounded-md p-2">
                 <x-chief::icon.arrow-left class="body-dark size-6" />
             </div>
 
@@ -55,16 +49,16 @@
         </div>
 
         <div>
-            @include('chief::templates.page.nav.nav-project')
-            @include('chief::templates.page.nav.nav-general')
+            @include ('chief::templates.page.nav.nav-project')
+            @include ('chief::templates.page.nav.nav-general')
         </div>
     </div>
 
     <div>
-        @include('chief::templates.page.nav.nav-settings')
-        @include('chief::templates.page.nav.nav-user')
+        @include ('chief::templates.page.nav.nav-settings')
+        @include ('chief::templates.page.nav.nav-user')
 
-        <p class="mt-3 px-2 text-xs text-grey-400">
+        <p class="text-grey-400 mt-3 px-2 text-xs">
             Je gebruikt momenteel Chief versie {{ \Composer\InstalledVersions::getVersion('thinktomorrow/chief') }}
         </p>
     </div>
